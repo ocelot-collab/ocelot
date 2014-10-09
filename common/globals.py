50128edfb00cbec9ef325a850fca7a0ba2b7d5e7
sergey-tomin <tomin.sergey@gmail.com>
2014-10-09 20:08:14 +0400

    added several small function for conversion of measures

--- common/globals.py
+++ common/globals.py
@@ -21,6 +21,53 @@ ro_e = 2.8179403267e-15
 Cgamma = 4.*pi/3.*ro_e/m_e_MeV**3
 Cq = 55./(32.*sqrt(3)*2*pi)*h_eV_s*speed_of_light/m_e_eV
 
+def lambda2eV(Lambda):
+    Eph = h_eV_s*speed_of_light/Lambda
+    return Eph
+
+def eV2lambda(Ephoton):
+    Lambda = h_eV_s*speed_of_light/Ephoton
+    return Lambda
+
+def Ephoton2K(Eph, lu = 0.04, Eeb = 14):
+    gamma = Eeb/m_e_GeV
+    K = sqrt(4.*gamma*gamma/lu*h_eV_s*speed_of_light/Eph - 2)
+    return K
+
+def K2Ephoton(K, lu = 0.04, E=14):
+    gamma = E/m_e_GeV
+    Eph = 4.*gamma*gamma*h_eV_s*speed_of_light/((K*K + 2)*lu)
+    return Eph
+
+def K2Lambda(K, lu = 0.04, E=14):
+    gamma = E/m_e_GeV
+    Eph = 4.*gamma*gamma*h_eV_s*speed_of_light/((K*K + 2)*lu)
+    Lambda = eV2lambda(Eph)
+    return Lambda
+
+def field2K(field, lu = 0.04):
+    K = field*lu*speed_of_light/(m_e_eV*2.*pi)
+    return K
+
+def K2field(K, lu = 0.04):
+    field = K*m_e_eV*2.*pi/(lu*speed_of_light)
+    return field
+
+def field2Ephoton(field, lu = 0.04, E=14):
+    gamma = E/m_e_GeV
+    K = field2K(field, lu)
+    l = lu/(2.*gamma*gamma)*(1.+K*K/2.)
+    Eph = h_eV_s*speed_of_light/l
+    return Eph
+
+def Ephoton2field(energy, lu = 0.04, Eeb = 14):
+    K = Ephoton2K(energy, lu, Eeb)
+    field = K*2.*pi*m_e_eV/(lu*speed_of_light)
+    return field
+
+
+
+
 
 Hz = 1.e-9
 KHz = 1.e-6

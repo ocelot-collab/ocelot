91258ca0c7472a2a34132fce85556efbdeb9af7e
sergey-tomin <tomin.sergey@gmail.com>
2015-06-15 13:13:06 +0200

    added RK method

--- cpbd/und_weave.py
+++ cpbd/und_weave.py
@@ -1,11 +1,179 @@
 __author__ = 'Sergey Tomin'
 
-#import numpy as np
+import numpy as np
 from numpy import sqrt, matrix, cos, sin, log, tan, eye, zeros, pi, array, linspace, dot, abs, random, arctan, sign
 from scipy import weave
+from scipy.integrate import odeint
 #import os
 #os.environ["CC"] = "gcc-4.8"
 #os.environ["CXX"] = "gcc-4.8"
+
+def moments(bx, by, Bx, By, Bz, dzk):
+    bx2 = bx*bx
+    by2 = by*by
+    bxy = bx*by
+    #sq = 1 + (bx2 + by2)/2.
+    sq = np.sqrt(1. + bx2 + by2)
+    k = sq*dzk
+    mx = k*(by*Bz - By*(1.+bx2) + bxy*Bx)
+    my = -k*(bx*Bz - Bx*(1.+by2) + bxy*By)
+    return mx, my
+
+
+def rk_track_in_field(y0, l, N, energy, mag_field):
+    z = np.linspace(0.,l, num=N)
+    h = z[1] - z[0]
+    N = len(z)
+    gamma = energy*1957.
+    charge = 1
+    mass = 1 #in electron mass
+    cmm = 299792458
+    massElectron = 0.510998910e+6 #// rest mass of electron
+
+    u = np.zeros((N*9,len(y0)/6))
+    px = y0[1::6]
+    py = y0[3::6]
+    dz = h
+    dGamma2 = 1. - 0.5/(gamma*gamma)
+    pz = dGamma2 - (px*px + py*py)/2.
+    k = charge*cmm/(massElectron*mass*gamma)
+    u[0,:] = y0[0::6]
+    u[1,:] = y0[1::6]
+    u[2,:] = y0[2::6]
+    u[3,:] = y0[3::6]
+    u[4,:] = 0.
+    u[5,:] = pz
+    dzk = dz*k
+    for i in range(N-1):
+        X = u[i*9 + 0]
+        Y = u[i*9 + 2]
+        Z = u[i*9 + 4]
+        bxconst = u[i*9 + 1]
+        byconst = u[i*9 + 3]
+        #bz = u[i*6 + 5]
+        bx = bxconst
+        by = byconst
+        kx1 = bx*dz
+        ky1 = by*dz
+        Bx, By, Bz = mag_field(X, Y, Z)
+        mx1, my1 = moments(bx, by, Bx, By, Bz, dzk)
+        u[i*9 + 6] = Bx
+        u[i*9 + 7] = By
+        u[i*9 + 8] = Bz
+        #K2
+        bx = bxconst + mx1/2.
+        by = byconst + my1/2.
+        kx2 = bx*dz
+        ky2 = by*dz
+        Bx, By, Bz = mag_field(X + kx1/2., Y + ky1/2., Z + dz/2.)
+        mx2, my2 = moments(bx, by, Bx, By, Bz, dzk)
+        # K3
+        bx = bxconst + mx2/2.
+        by = byconst + my2/2.
+        kx3 = bx*dz
+        ky3 = by*dz
+        Bx, By, Bz = mag_field(X + kx2/2., Y + ky2/2., Z + dz/2.)
+        mx3, my3 = moments(bx, by, Bx, By, Bz, dzk)
+        #K4
+        Z_n = Z + dz
+        bx = bxconst + mx3
+        by = byconst + my3
+        kx4 = bx*dz
+        ky4 = by*dz
+        Bx, By, Bz = mag_field(X + kx3, Y + ky3, Z_n)
+        mx4, my4 = moments(bx, by, Bx, By, Bz, dzk)
+
+        u[(i+1)*9 + 0] = X + 1/6.*(kx1 + 2.*(kx2 + kx3) + kx4)
+        u[(i+1)*9 + 1] = bxconst + 1/6.*(mx1 + 2.*(mx2 + mx3) + mx4) #// conversion in mrad
+        u[(i+1)*9 + 2] = Y + 1/6.*(ky1 + 2.*(ky2 + ky3) + ky4)
+        u[(i+1)*9 + 3] = byconst + 1/6.*(my1 + 2.*(my2 + my3) + my4)
+        u[(i+1)*9 + 4] = Z_n
+        u[(i+1)*9 + 5] = dGamma2 - (u[(i+1)*9 + 1]*u[(i+1)*9 + 1] + u[(i+1)*9 + 3]*u[(i+1)*9 + 3])/2.
+
+    u[(N-1)*9 + 6], u[(N-1)*9 + 7], u[(N-1)*9 + 8] = mag_field(u[(N-1)*9 + 0], u[(N-1)*9 + 2], u[(N-1)*9 + 4])
+    return u
+
+def scipy_track_in_field(y0, l, N, energy, mag_field):# y0, l, N, energy, mag_field
+    z = np.linspace(0.,l, num=N)
+    def func(y, z, fields):
+        gamma = energy*1957.
+        charge = 1
+        mass = 1 #in electron mass
+        cmm = 299792458
+        massElectron = 0.510998910e+6 #// rest mass of electron
+        k = charge*cmm/(massElectron*mass*gamma)
+        n = len(y)/4
+        X = y[0:n]
+        bx = y[n:2*n]
+        Y = y[2*n:3*n]
+        by = y[3*n:4*n]
+        bx2 = bx*bx
+        by2 = by*by
+        bxy = bx*by
+        #sq = 1 + (bx2 + by2)/2.
+        sq = np.sqrt(1. + bx2 + by2)
+        k = sq*k
+        Bx, By, Bz = fields(X, Y, z)
+        mx = k*(by*Bz - By*(1.+bx2) + bxy*Bx)
+        my = -k*(bx*Bz - Bx*(1.+by2) + bxy*By)
+        y = np.append(bx, (mx, by, my))
+        return array(y) #[y[1],mx, y[3], my]
+
+    n = len(y0)/6
+    x = y0[0::6]
+    bx = y0[1::6]
+    y = y0[2::6]
+    by = y0[3::6]
+    Y0 = np.append(x, (bx, y, by))
+
+    u = odeint(func, Y0, z, args = (mag_field,), rtol=1e-10, atol=1e-10)
+
+    w = np.zeros((6*len(z), n))
+    w[0::6] = u[:, 0:n]
+    w[1::6] = u[:, n:2*n]
+    w[2::6] = u[:, 2*n:3*n]
+    w[3::6] = u[:, 3*n:4*n]
+    N = len(z)
+    sub = y0[4::6]
+    sub = np.tile(sub, N).reshape((N, n))
+    w[4::6] = sub[:]
+    sub = y0[5::6]
+    sub = np.tile(sub, N).reshape((N, n))
+    w[5::6] = sub[:]
+    return w
+
+
+def rk_field(y0, l, N, energy, mag_field):
+    #z = linspace(0, l, num=N)
+    #h = z[1]-z[0]
+    #N = len(z)
+    traj_data = rk_track_in_field(y0, l, N, energy, mag_field)
+    #print np.shape(traj_data), np.shape(y0)
+    #print traj_data
+    y0[0::6] = traj_data[(N-1)*9 + 0,:]
+    y0[1::6] = traj_data[(N-1)*9 + 1,:]
+    y0[2::6] = traj_data[(N-1)*9 + 2,:]
+    y0[3::6] = traj_data[(N-1)*9 + 3,:]
+    #y0[4::6] = traj_data[(N-1)*9 + 4,:]
+    #y0[5::6] = traj_data[(N-1)*9 + 5,:]
+    return y0
+
+def rk_field_scipy(y0, l, N, energy, mag_field):
+    #z = linspace(0, l, num=N)
+    #h = z[1]-z[0]
+    #N = len(z)
+    traj_data = scipy_track_in_field(y0, l, N, energy, mag_field)
+    #print np.shape(traj_data), np.shape(y0)
+    #print traj_data
+    y0[0::6] = traj_data[(N-1)*6 + 0,:]
+    y0[1::6] = traj_data[(N-1)*6 + 1,:]
+    y0[2::6] = traj_data[(N-1)*6 + 2,:]
+    y0[3::6] = traj_data[(N-1)*6 + 3,:]
+    #y0[4::6] = traj_data[(N-1)*9 + 4,:]
+    #y0[5::6] = traj_data[(N-1)*9 + 5,:]
+    return y0
+
+
 def track_und_sym(y0, z, kz, kx, Kx, energy):
     gamma = energy*1957.
     #if energy == 0:
@@ -172,12 +340,41 @@ def track_und_RK(y0, z, kz, kx ,Kx, energy):
     #print N
 
     u = zeros(N*6)
+    support_code = """
+    extern "C" {
+    void fields(double x, double y, double z, double kx, double ky, double kz, double B0, double *Bx, double *By, double *Bz)
+    {
+        double k1 =  -B0*kx/ky;
+        double k2 = -B0*kz/ky;
+        double kx_x = kx*x;
+        double ky_y = ky*y;
+        double kz_z = kz*z;
+        double cosx = cos(kx_x);
+        double sinhy = sinh(ky_y);
+        double cosz = cos(kz_z);
+        *Bx = k1*sin(kx_x)*sinhy*cosz ;
+        *By = B0*cosx*cosh(ky_y)*cosz;
+        *Bz = k2*cosx*sinhy*sin(kz_z);
+    };
+    void moments(double bx, double by, double Bx, double By, double Bz, double dzk, double *mx, double *my)
+    {
+        double bx2 = bx*bx;
+        double by2 = by*by;
+        double bxy = bx*by;
+        //#sq = 1 + (bx2 + by2)/2.
+        double k = sqrt(1. + bx2 + by2)*dzk;
+        *mx = k*(by*Bz - By*(1.+bx2) + bxy*Bx);
+        *my = -k*(bx*Bz - Bx*(1.+by2) + bxy*By);
+    }
+    }
+    """
     code = """
+
     double charge = 1;
     double mass = 1; //in electron mass
 
     double X, Y, Z;
-    double bx, by, bz;
+    double bx, by;
     double kx1, ky1;
     double kx2, ky2;
     double kx3, ky3;
@@ -190,22 +387,16 @@ def track_und_RK(y0, z, kz, kx ,Kx, energy):
     double byconst;
 
     double cmm = 299792458;
-    double k;
+    //double k;
     double massElectron = 0.510998910e+6; // rest mass of electron
     double dzk ;
     double Bx;
     double By;
     double Bz;
 
-    double bx2;
-    double by2;
 
-    double x = Q1(0);
     double px = Q1(1);
-    double y = Q1(2);
     double py = Q1(3);
-    //double tau = Q1(4);
-    //double delta = Q1(5);
     double kx = Q1(6);
     double ky = Q1(7);
     double kz = Q1(8);
@@ -214,20 +405,18 @@ def track_und_RK(y0, z, kz, kx ,Kx, energy):
     int N = Q1(10);
     double B0 = Q1(11);
     double gamma = Q1(12);
-    double z = 0.;
     double dGamma2 = 1. - 0.5/(gamma*gamma);
     double pz = dGamma2 - (px*px + py*py)/2.;
 
-    double sq;
     int i;
 
-    k = charge*cmm/(massElectron*mass*gamma);
+    double k = charge*cmm/(massElectron*mass*gamma);
 
     U1(0) = Q1(0);
     U1(1) = Q1(1);
     U1(2) = Q1(2);
     U1(3) = Q1(3);
-    U1(4) = z;
+    U1(4) = 0.;
     U1(5) = pz;
     dzk = dz*k;
     for(i = 0; i < N-1; i++)
@@ -240,92 +429,35 @@ def track_und_RK(y0, z, kz, kx ,Kx, energy):
 
         bxconst = U1(i*6 + 1);
         byconst = U1(i*6 + 3);
-        bz = U1(i*6 + 5);
 
         bx = bxconst;
         by = byconst;
-
-        z = Z;
-        x = X;
-        y = Y;
-        Bx = -B0*kx/ky*sin(kx*x)*sinh(ky*y)*cos(kz*z); // here kx is only real
-        By = B0*cos(kx*x)*cosh(ky*y)*cos(kz*z);
-        Bz = -B0*kz/ky*cos(kx*x)*sinh(ky*y)*sin(kz*z);
-
-        //motion->XbetaI2[i] = motion->By[i];
         kx1 = bx*dz;
         ky1 = by*dz;
-        bx2 = bx*bx;
-        by2 = by*by;
-        //sq = 1 + (bx2 + by2)/2.;
-        sq = sqrt(1 + bx2 + by2);
 
-        mx1 = sq*(by*Bz - By*(1+bx2) + bx*by*Bx)*dzk;
-        my1 = -sq*(bx*Bz - Bx*(1+by2) + bx*by*By)*dzk;
+        fields( X,  Y,  Z,  kx,  ky,  kz,  B0, &Bx, &By, &Bz);
+        moments( bx,  by,  Bx,  By,  Bz,  dzk,  &mx1,  &my1);
         //K2
-        //err = B3D(field, X + kx1/2., Y + ky1/2., Z + dz/2. + Zshift, pBx, pBy, pBz);
-        x = X + kx1/2.;
-        y = Y + ky1/2.;
-        z = Z + dz/2.;
-        Bx = -B0*kx/ky*sin(kx*x)*sinh(ky*y)*cos(kz*z); // here kx is only real
-        By = B0*cos(kx*x)*cosh(ky*y)*cos(kz*z);
-        Bz = -B0*kz/ky*cos(kx*x)*sinh(ky*y)*sin(kz*z);
-
         bx = bxconst + mx1/2.;
         by = byconst + my1/2.;
-
         kx2 = bx*dz;
         ky2 = by*dz;
-        bx2 = bx*bx;
-        by2 = by*by;
-        //sq = 1 + (bx2 + by2)/2.;
-        sq = sqrt(1 + bx*bx + by*by);
-        mx2 = sq*(by*Bz - By*(1+bx2) + bx*by*Bx)*dzk;
-        my2 = -sq*(bx*Bz - Bx*(1+by2) + bx*by*By)*dzk;
+        fields( X + kx1/2.,  Y + ky1/2.,  Z + dz/2.,  kx,  ky,  kz,  B0, &Bx, &By, &Bz);
+        moments( bx,  by,  Bx,  By,  Bz,  dzk,  &mx2,  &my2);
         // K3
-        //err = B3D(field, X + kx2/2., Y + ky2/2., Z + dz/2. + Zshift, pBx, pBy, pBz);
-
-        x = X + kx2/2.;
-        y = Y + ky2/2.;
-        z = Z + dz/2.;
-        Bx = -B0*kx/ky*sin(kx*x)*sinh(ky*y)*cos(kz*z); // here kx is only real
-        By = B0*cos(kx*x)*cosh(ky*y)*cos(kz*z);
-        Bz = -B0*kz/ky*cos(kx*x)*sinh(ky*y)*sin(kz*z);
-
         bx = bxconst + mx2/2.;
         by = byconst + my2/2.;
-
         kx3 = bx*dz;
         ky3 = by*dz;
-        bx2 = bx*bx;
-        by2 = by*by;
-        //sq = 1 + (bx2 + by2)/2.;
-        sq = sqrt(1 + bx*bx + by*by);
-        mx3 = sq*(by*Bz - By*(1+bx2) + bx*by*Bx)*dzk;
-        my3 = -sq*(bx*Bz - Bx*(1+by2) + bx*by*By)*dzk;
-
+        fields( X + kx2/2.,  Y + ky2/2.,  Z + dz/2.,  kx,  ky,  kz,  B0, &Bx, &By, &Bz);
+        moments( bx,  by,  Bx,  By,  Bz,  dzk,  &mx3,  &my3);
         //K4
-        //err = B3D(field, X + kx3, Y + ky3, Z + dz + Zshift,pBx, pBy, pBz);
-
-        x = X + kx3;
-        y = Y + ky3;
-        z = Z + dz;
-        Bx = -B0*kx/ky*sin(kx*x)*sinh(ky*y)*cos(kz*z); // here kx is only real
-        By = B0*cos(kx*x)*cosh(ky*y)*cos(kz*z);
-        Bz = -B0*kz/ky*cos(kx*x)*sinh(ky*y)*sin(kz*z);
-
         bx = bxconst + mx3;
         by = byconst + my3;
-
-
         kx4 = bx*dz;
         ky4 = by*dz;
-        bx2 = bx*bx;
-        by2 = by*by;
-        //sq = 1 + (bx2 + by2)/2.;
-        sq = sqrt(1 + bx*bx + by*by);
-        mx4 = sq*(by*Bz - By*(1+bx2) + bx*by*Bx)*dzk;
-        my4 = -sq*(bx*Bz - Bx*(1+by2) + bx*by*By)*dzk;
+        fields( X + kx3,  Y + ky3,  Z + dz,  kx,  ky,  kz,  B0, &Bx, &By, &Bz);
+        moments( bx,  by,  Bx,  By,  Bz,  dzk,  &mx4,  &my4);
 
         U1((i+1)*6 + 0) = X + 1/6.*(kx1 + 2.*kx2 + 2.*kx3 + kx4);
         U1((i+1)*6 + 1) = bxconst + 1/6.*(mx1 + 2.*mx2 + 2.*mx3 + mx4); // conversion in mrad
@@ -345,7 +477,7 @@ def track_und_RK(y0, z, kz, kx ,Kx, energy):
 
 
     """
-    weave.inline(code, ['u',"q"])
+    weave.inline(code, ['u',"q"], support_code= support_code)
     x = u[::6]
     y = u[2::6]
     #print x

c6e259fe82b0d8b926872134cb54b04235bdb64b
sergey-tomin <tomin.sergey@gmail.com>
2015-09-08 17:16:55 +0200

    order

--- demos/ebeam/da_mpi.py
+++ demos/ebeam/da_mpi.py
@@ -52,7 +52,7 @@ x_array = linspace(-0.03, 0.03, nx)
 y_array = linspace(0.0001, 0.03, ny)
 start = time()
 pxy_list = create_track_list(x_array, y_array, p_array=[0.])
-pxy_list = tracking_mpi( mpi_comm,lat, nturns, pxy_list,  nsuperperiods = 8, save_track=False)
+pxy_list = tracking_mpi( mpi_comm,lat, nturns, pxy_list,  nsuperperiods = 8, order = 3, save_track=False)
 if rank == 0:
     print( time() - start)
     da = array(map(lambda pxy: pxy.turn, pxy_list))

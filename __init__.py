7b6c06d67b9816d3ff3ce6419c075ac17d7b405b
Ilya Agapov <ilya.agapov@xfel.eu>
2015-11-19 15:54:36 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-__version__ = '15.11-rc2'
+__version__ = '15.11'
 
 __all__ = ['Twiss', 'twiss', "Beam", "Particle", "get_current", "get_envelope",                 # beam
            'fodo_parameters', 'lattice_transfer_map', 'TransferMap', 'gauss_from_twiss',        # optics

01b6a0e60e4bf77fab93418388c252052b518da6
sergey-tomin <tomin.sergey@gmail.com>
2016-01-20 20:28:33 +0100

    minor

--- cpbd/orbit_correction.py
+++ cpbd/orbit_correction.py
@@ -179,7 +179,7 @@ class Orbit:
         L = 0.
         for bpm in self.bpms:
             dz = bpm.s - L
-            track(lattice, [p], dz, navi, order=2)
+            track(lattice, [p], dz, navi, order=1)
             bpm.x = p.x
             bpm.y = p.y
             bpm.E = p.E

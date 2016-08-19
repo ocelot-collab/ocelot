66f3dc539a2268f529ee7e342eb999f9b31432d9
takanori <tak.tanikawa@gmail.com>
2016-08-19 17:34:03 +0200

    re-add rad_file creation function disappeared

--- adaptors/genesis.py
+++ adaptors/genesis.py
@@ -1803,3 +1803,26 @@ def test_beam_transform(beta1=10.0, alpha1=-0.1, beta2=20, alpha2=2.2):
 #parser.add_argument('input', help='input file')
 #args = parser.parse_args()
 
+
+def create_rad_file(p_duration_s = None, p_intensity = None, beam = None, offset = None, out_file = 'tmp.rad'):
+    temporal_delay = offset
+    extention_twindow = 2
+    start = beam.z[0]-extention_twindow*(beam.z[math.floor(len(beam.z)/2)]-beam.z[0])
+    stop = beam.z[len(beam.z)-1]+extention_twindow*(beam.z[len(beam.z)-1]-beam.z[math.ceil(len(beam.z)/2)])
+    num = len(beam.z)*(1+extention_twindow)
+    intensity = np.empty(num)
+    p_duration_m = p_duration_s*299792458
+    
+    class radfileparams:
+        offset = temporal_delay
+        offset2 = start
+        z = np.linspace(start, stop, num)
+        for i in range(num):
+            intensity[i] = p_intensity*math.exp(-(z[i]-offset)**2/(2*p_duration_m**2)) 
+        prad0 = intensity
+        
+    rad = radfileparams()
+    f = open(out_file,'w')
+    f.write(rad_file_str2(rad))
+    f.close()
+

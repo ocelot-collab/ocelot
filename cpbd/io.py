a14752857542a9cf64b03214b49035e4abd5b665
sergey-tomin <tomin.sergey@gmail.com>
2016-09-24 17:19:37 -0700

    some improvements

--- cpbd/io.py
+++ cpbd/io.py
@@ -23,44 +23,81 @@ def find_drifts(lat):
 
 
 def find_objects(lat, types):
+    """
+    Function finds objects by types and adds it to list if object is unique.
+    :param lat: MagneticLattice
+    :param types: types of the Elements
+    :return: list of elements
+    """
     obj_id = []
     objs = []
     for elem in lat.sequence:
         if elem.__class__ in types:
-            if elem.id not in obj_id:
+            if id(elem) not in obj_id:
                 objs.append(elem)
-                obj_id.append(elem.id)
+                obj_id.append(id(elem))
+
     return objs
 
+def create_var_name(objects):
+    alphabet = "abcdefgiklmn"
+    ids = [obj.id for obj in objects]
+    search_occur = lambda obj_list, name: [i for i, x in enumerate(obj_list) if x == name]
+    for j, obj in enumerate(objects):
+        inx = search_occur(ids, obj.id)
+        if len(inx) > 1:
+            for n, i in enumerate(inx):
+                name = ids[i]
+                name = name.replace('.','_')
+                name = name.replace(':','_')
+                ids[i] = name + alphabet[n]
+        else:
+            name = ids[j]
+            name = name.replace('.','_')
+            name = name.replace(':','_')
+            ids[j] = name
+        obj.name = ids[j]
 
+    return objects
+
+
+def find_obj_and_create_name(lat, types):
+    objects = find_objects(lat, types=types)
+    objects = create_var_name(objects)
+    return objects
 
 def lat2input(lat):
     """
     returns python input string for the lattice in the lat object
     """
-    drifts = find_objects(lat, types = [Drift])
-    quads = find_objects(lat, types = [Quadrupole])
-    sexts = find_objects(lat, types = [Sextupole])
-    octs = find_objects(lat, types = [Octupole])
-    cavs = find_objects(lat, types = [Cavity])
-    sols = find_objects(lat, types = [Solenoid])
-    matrices = find_objects(lat, types = [Matrix])
-    marks = find_objects(lat, types = [Marker])
-    mons = find_objects(lat, types = [Monitor])
-    unds = find_objects(lat, types = [Undulator])
-    cors = find_objects(lat, types = [Hcor, Vcor])
-    bends = find_objects(lat, types = [Bend, RBend, SBend])
+    drifts = find_obj_and_create_name(lat, types = [Drift])
+    quads = find_obj_and_create_name(lat, types = [Quadrupole])
+    sexts = find_obj_and_create_name(lat, types = [Sextupole])
+    octs = find_obj_and_create_name(lat, types = [Octupole])
+    cavs = find_obj_and_create_name(lat, types = [Cavity])
+    sols = find_obj_and_create_name(lat, types = [Solenoid])
+    matrices = find_obj_and_create_name(lat, types = [Matrix])
+    marks = find_obj_and_create_name(lat, types = [Marker])
+    mons = find_obj_and_create_name(lat, types = [Monitor])
+    unds = find_obj_and_create_name(lat, types = [Undulator])
+    cors = find_obj_and_create_name(lat, types = [Hcor, Vcor])
+    bends = find_obj_and_create_name(lat, types = [Bend, RBend, SBend])
+    unkns = find_obj_and_create_name(lat, types = [UnknownElement])
     #end find objects
 
-    lines = []
+    lines = ["from ocelot import * \n"]
     lines.append("\n# drifts \n")
     for drift in drifts:
-        line = drift.id.replace('.','_') + " = Drift(l = " + str(drift.l)+ ", eid = '"+ drift.id+ "')\n"
+        #name = drift.id.replace('.','_')
+        #name = name.replace(':','_')
+        line = drift.name.lower() + " = Drift(l=" + str(drift.l)+ ", eid='"+ drift.id+ "')\n"
         lines.append(line)
 
     lines.append("\n# quadrupoles \n")
     for quad in quads:
-        line = quad.id.replace('.','_') + " = Quadrupole(l = " + str(quad.l) +", k1 = "+ str(quad.k1) +", tilt = "+ str(quad.tilt)  +", eid = '"+ quad.id+ "')\n"
+        #name = quad.id.replace('.','_')
+        #name = name.replace(':','_')
+        line = quad.name.lower() + " = Quadrupole(l=" + str(quad.l) +", k1="+ str(quad.k1) +", tilt="+ str(quad.tilt)  +", eid='"+ quad.id+ "')\n"
         lines.append(line)
 
     lines.append("\n# bending magnets \n")
@@ -75,11 +112,15 @@ def lat2input(lat):
             k = ''
         else:
             k = ", k1 = "+ str(bend.k1)
-        fint = bend.fint1
+        #fint = bend.fint1
         #if bend.fint1 == bend.fint2:
         #    fint = bend.fint1
 
-        line = bend.id.replace('.','_') + type + str(bend.l) + k + ", angle = " + str(bend.angle)+ ", e1 = " + str(bend.e1) + ", e2 = " + str(bend.e2) + ", tilt = " + str(bend.tilt) + ", fint = " + str(fint) +", eid = '"+ bend.id+ "')\n"
+        #name = bend.id.replace('.','_')
+        #name = name.replace(':','_')
+        line = bend.name.lower() + type + str(bend.l) + k + ", angle = " + str(bend.angle)+\
+               ", e1 = " + str(bend.e1) + ", e2 = " + str(bend.e2)+ ", gap = " + str(bend.gap)  + \
+               ", tilt = " + str(bend.tilt) + ", fint = " + str(bend.fint)  + ", fintx = " + str(bend.fintx) +", eid = '"+ bend.id+ "')\n"
         lines.append(line)
 
     lines.append("\n# correctors \n")
@@ -88,40 +129,60 @@ def lat2input(lat):
             type = " = Hcor(l = "
         else:
             type = " = Vcor(l = "
-        line = cor.id.replace('.','_') + type + str(cor.l) + ", angle = "+ str(cor.angle) +", eid = '"+ cor.id+ "')\n"
+        #name = cor.id.replace('.','_')
+        #name = name.replace(':','_')
+        line = cor.name.lower() + type + str(cor.l) + ", angle = "+ str(cor.angle) +", eid = '"+ cor.id+ "')\n"
         lines.append(line)
 
     lines.append("\n# markers \n")
     for mark in marks:
-        line = mark.id.replace('.','_') + " = Marker(eid = '"+ mark.id+ "')\n"
+        #name = mark.id.replace('.','_')
+        #name = name.replace(':','_')
+        line = mark.name.lower() + " = Marker(eid = '"+ mark.id+ "')\n"
         lines.append(line)
 
     lines.append("\n# monitor \n")
     for mon in mons:
-        line = mon.id.replace('.','_') + " = Monitor(eid = '"+ mon.id+ "')\n"
+        #name = mon.id.replace('.','_')
+        #name = name.replace(':','_')
+        line = mon.name.lower() + " = Monitor(eid = '"+ mon.id+ "')\n"
         lines.append(line)
 
     lines.append("\n# sextupoles \n")
     for sext in sexts:
-        line = sext.id.replace('.','_') + " = Sextupole(l = " + str(sext.l) +", k2 = "+ str(sext.k2) +", tilt = "+ str(sext.tilt) +", eid = '"+ sext.id+ "')\n"
+        #name = sext.id.replace('.','_')
+        #name = name.replace(':','_')
+        line = sext.name.lower() +" = Sextupole(l = " + str(sext.l) +", k2 = "+ str(sext.k2) +", tilt = "+ str(sext.tilt) +", eid = '"+ sext.id+ "')\n"
         lines.append(line)
 
     lines.append("\n# octupole \n")
     for oct in octs:
-        line = oct.id.replace('.','_') + " = Octupole(l = " + str(oct.l) +", k3 = "+ str(oct.k3) +", tilt = "+ str(oct.tilt) +", eid = '"+ oct.id+ "')\n"
+        #name = oct.id.replace('.','_')
+        #name = name.replace(':','_')
+        line = oct.name.lower() + " = Octupole(l = " + str(oct.l) +", k3 = "+ str(oct.k3) +", tilt = "+ str(oct.tilt) +", eid = '"+ oct.id+ "')\n"
         lines.append(line)
 
     lines.append("\n# undulator \n")
     for und in unds:
-        line = und.id.replace('.','_') + " = Undulator(lperiod = " + str(und.lperiod) +", nperiods = "+ str(und.nperiods) +", Kx = "+ str(und.Kx) +", Ky = "+ str(und.Ky) +", eid = '"+ und.id+ "')\n"
+        #name = und.id.replace('.','_')
+        #name = name.replace(':','_')
+        line = und.name.lower() +  " = Undulator(lperiod = " + str(und.lperiod) +", nperiods = "+ str(und.nperiods) +", Kx = "+ str(und.Kx) +", Ky = "+ str(und.Ky) +", eid = '"+ und.id+ "')\n"
         lines.append(line)
 
     lines.append("\n# cavity \n")
     for cav in cavs:
-        line = cav.id.replace('.','_') + " = Cavity(l = " + str(cav.l) + ", v = "+ str(cav.v) +\
+        #name = cav.id.replace('.','_')
+        #name = name.replace(':','_')
+        line = cav.name.lower() + " = Cavity(l = " + str(cav.l) + ", v = "+ str(cav.v) +\
                ", freq = "+ str(cav.f) +", phi = "+ str(cav.phi) + ", eid = '"+ cav.id+ "')\n"
         lines.append(line)
 
+    lines.append("\n# UnknowElement \n")
+    for unkn in unkns:
+        #name = unkn.id.replace('.','_')
+        #name = name.replace(':','_')
+        line = unkn.name.lower() +  " = UnknownElement(l = " + str(unkn.l) +  ", eid = '"+ unkn.id+ "')\n"
+        lines.append(line)
     #lines.append("\n# rfcavity \n")
     #for rfcav in rfcavs:
     #    line = rfcav.id.replace('.','_') + " = RFcavity(l = " + str(rfcav.l) +", volt = "+ str(rfcav.volt) +", lag = "+ str(rfcav.lag)+\
@@ -131,7 +192,9 @@ def lat2input(lat):
     lines.append("\n# Matrices \n")
 
     for mat in matrices:
-        line = mat.id.replace('.','_') + " = Matrix(l = " + str(mat.l) +\
+        #name = mat.id.replace('.','_')
+        #name = name.replace(':','_')
+        line = mat.name.lower() + " = Matrix(l = " + str(mat.l) +\
                ", rm11 = "+ str(mat.rm11) + ", rm12 = "+ str(mat.rm12) + ", rm13 = "+ str(mat.rm13) + ", rm14 = "+ str(mat.rm14) +\
                ", rm21 = "+ str(mat.rm21) + ", rm22 = "+ str(mat.rm22) + ", rm23 = "+ str(mat.rm23) + ", rm24 = "+ str(mat.rm24) +\
                ", rm31 = "+ str(mat.rm31) + ", rm32 = "+ str(mat.rm32) + ", rm33 = "+ str(mat.rm33) + ", rm34 = "+ str(mat.rm34) +\
@@ -142,7 +205,9 @@ def lat2input(lat):
     lines.append("\n# Solenoids \n")
 
     for sol in sols:
-        line = sol.id.replace('.','_') + " = Solenoid(l = " + str(sol.l) +", k = "+ str(sol.k) +", eid = '"+ sol.id+ "')\n"
+        #name = sol.id.replace('.','_')
+        #name = name.replace(':','_')
+        line = sol.name.lower() + " = Solenoid(l = " + str(sol.l) +", k = "+ str(sol.k) +", eid = '"+ sol.id+ "')\n"
         lines.append(line)
 
     lines.append("\n# lattice \n")
@@ -150,7 +215,9 @@ def lat2input(lat):
     for elem in lat.sequence:
         #print elem.id, elem.type
         if elem.__class__ != Edge:
-            names.append(elem.id.replace('.','_'))
+            #name = elem.id.replace('.','_')
+            #name = name.replace(':','_')
+            names.append(elem.name.lower())
     #names = map(lambda p: p.id, lat.sequence)
     new_names = []
     for i, name in enumerate(names):

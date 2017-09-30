updated elegant2ocelot. fixed minor bug in orbit correction (epsilon_x(y)) added new method DRM calculation added setup.py for testing

adaptors/elegant2ocelot.py
@@ -128,12 +128,12 @@ def read_lattice_elegant(file_flo, file_par):
         if pos>n_par-2:
             break
         elem.l=eval(data_par[pos][2])
-        if elem.type=="quadrupole":
+        if elem.__class__ == Quadrupole:
             elem.k1=eval(data_par[pos+1][2])
             elem.tilt = eval(data_par[pos+2][2])
-        elif elem.type=="sextupole":
+        elif elem.__class__ == Sextupole:
             elem.k2=eval(data_par[pos+1][2])
-        elif elem.type=="bend":
+        elif elem.__class__ == Bend:
             elem.angle=eval(data_par[pos+1][2])
             if stype in ['CRBEND', 'CSRCRBEND', 'CSRCSBEND', "CSBEND"]:
                 elem.e1 = eval(data_par[pos+10][2])
@@ -145,10 +145,10 @@ def read_lattice_elegant(file_flo, file_par):
                 elem.e1=eval(data_par[pos+3][2])
                 elem.e2=eval(data_par[pos+4][2])
                 elem.tilt=eval(data_par[pos+5][2])
-        elif elem.type=="rbend":
+        elif elem.__class__ == RBend:
             elem.angle=eval(data_par[pos+1][2])
             elem.tilt=eval(data_par[pos+12][2])
-        elif elem.type=="undulator":
+        elif elem.__class__ == Undulator:
             elem.l=eval(data_par[pos][2])
             #print( elem.l)
             elem.nperiods=eval(data_par[pos+8][2])/2
@@ -156,21 +156,21 @@ def read_lattice_elegant(file_flo, file_par):
             elem.lperiod=elem.l/elem.nperiods
             elem.Kx=eval(data_par[pos+2][2])
             elem.Ky=0
-        elif elem.type=="cavity":
+        elif elem.__class__ == Cavity:
             elem.l=eval(data_par[pos][2])
             elem.v=eval(data_par[pos+1][2])*1e-9  # V -> GV
             elem.phi=(eval(data_par[pos+2][2])-90)#/180.*np.pi
             elem.f=eval(data_par[pos+3][2])
             elem.delta_e=elem.v*cos(elem.phi)  # in GeV
-        elif elem.type=="hcor":
+        elif elem.__class__ == Hcor:
             elem.l=eval(data_par[pos][2])
-        elif elem.type=="vcor":
+        elif elem.__class__ == Vcor:
             elem.l=eval(data_par[pos][2])
-        elif elem.type=="monitor":
+        elif elem.__class__ == Monitor:
             elem.l=eval(data_par[pos][2])
-        elif elem.type=="marker":
+        elif elem.__class__ == Marker:
             elem.l= 0. # eval(data_par[pos][2])
-        elif elem.type=="drift":
+        elif elem.__class__ == Drift:
             elem.l=eval(data_par[pos][2])
         elem.id = elem.id.replace('.','_')
         elem.id = elem.id.replace('[','_')

cpbd/optics.py
@@ -809,7 +809,6 @@ def lattice_transfer_map(lattice, energy):
     Ta = np.zeros((6, 6, 6))
     E = energy
     for i, elem in enumerate(lattice.sequence):
-
         Rb = elem.transfer_map.R(E)
         if lattice.method.global_method == SecondTM:
             Tc = np.zeros((6, 6, 6))
@@ -822,7 +821,6 @@ def lattice_transfer_map(lattice, energy):
                         t2 = 0.
                         for l in range(6):
                             t1 += Rb[i, l] * Ta[l, j, k]
-
                             for m in range(6):
                                 t2 += Tb[i, l, m] * Ra[l, j] * Ra[m, k]
                         Tc[i, j, k] = t1 + t2
@@ -834,6 +832,24 @@ def lattice_transfer_map(lattice, energy):
     lattice.T = unsym_matrix(deepcopy(Ta))
     lattice.R = Ra
     return Ra
+
+
+def second_order_mult(Ra, Ta, Rb, Tb, sym_flag=True):
+    Rc = np.dot(Rb, Ra)
+    Tc = np.zeros((6, 6, 6))
+    Tb = sym_matrix(Tb)
+    for i in range(6):
+        for j in range(6):
+            for k in range(6):
+                t1 = 0.
+                t2 = 0.
+                for l in range(6):
+                    t1 += Rb[i, l] * Ta[l, j, k]
+                    for m in range(6):
+                        t2 += Tb[i, l, m] * Ra[l, j] * Ra[m, k]
+                Tc[i, j, k] = t1 + t2
+    Tc = unsym_matrix(deepcopy(Tc))
+    return Rc, Tc
 
 
 def trace_z(lattice, obj0, z_array):

cpbd/orbit_correction.py
@@ -1,11 +1,6 @@
 __author__ = 'Sergey Tomin'
 
 
-#import pickle
-from time import sleep
-
-#import matplotlib.pyplot as plt
-#from ocelot.gui.accelerator import *
 from numpy import diag, shape
 from numpy.linalg import svd
 from scipy.interpolate import splrep, splev
@@ -16,6 +11,7 @@ from ocelot.cpbd.track import *
 from ocelot.cpbd.response_matrix import *
 import copy
 import json
+from time import sleep, time
 
 
 
@@ -253,7 +249,7 @@ class NewOrbit:
         bpm_weights_diag = np.diag( np.append(bpm_weights, [bpm_weights, bpm_weights, bpm_weights]))
         #print("bpm_weights = ", np.shape(bpm_weights), len(orbit))
 
-        self.orbit_svd = OrbitSVD(resp_matrix=rmatrix, orbit=orbit, weights=bpm_weights_diag, epsilon_x=epsilon_x, epsilon_y=epsilon_x)
+        self.orbit_svd = OrbitSVD(resp_matrix=rmatrix, orbit=orbit, weights=bpm_weights_diag, epsilon_x=epsilon_x, epsilon_y=epsilon_y)
 
         #self.orbit_svd = LInfinityNorm(resp_matrix=rmatrix, orbit=orbit, weights=bpm_weights_diag, epsilon_x=epsilon_x,
         #                          epsilon_y=epsilon_x)

cpbd/response_matrix.py
@@ -367,7 +367,38 @@ class LinacDisperseTmatrixRM(MeasureResponseMatrix):
         :param tw_init: if tw_init == None, function tries to find periodical solution
         :return: orbit.resp
         """
-        pass
+        if tw_init == None:
+            print("ADD INITIAL TWISS TO LinacDisperseTmatrixRM.calculate(tw_init)")
+        m = len(self.bpms)
+        nx = len(self.hcors)
+        ny = len(self.vcors)
+        self.resp = zeros((2 * m, nx + ny))
+
+        for j, cor in enumerate([item for sublist in [self.hcors, self.vcors] for item in sublist]):
+            print(j, "/", nx + ny, cor.id)
+            Ra = np.eye(6)
+            Ta = np.zeros((6, 6, 6))
+            E = tw_init.E
+            for i, elem in enumerate(self.lat.sequence):
+                if i < cor.lat_inx:
+                    E += elem.transfer_map.delta_e
+                    continue
+                #Tc = np.zeros((6, 6, 6))
+                Rb = elem.transfer_map.R(E)
+                Tb = deepcopy(elem.transfer_map.t_mat_z_e(elem.l, E))
+                #Ra = dot(Rb, Ra)
+                Ra, Ta = second_order_mult(Ra, Ta, Rb, Tb)
+                E += elem.transfer_map.delta_e
+                if elem in self.bpms:
+
+                    n = self.bpms.index(elem)
+
+                    if cor.__class__ == Hcor:
+                        self.resp[n, j] = Ta[0, 1, 5]
+
+                    else:
+                        self.resp[n + m, j] = Ra[2, 3, 5]
+        return self.resp
 
 
 class ResponseMatrix:
@@ -493,7 +524,7 @@ class ResponseMatrix:
         dict_rmatrix["method_name"] = self.method.__class__.__name__ if self.method != None else "None"
         dict_rmatrix["mode"] = self.mode
 
-        with open(filename, 'w') as f:
+        with open(filename, 'w+') as f:
             json.dump(dict_rmatrix, f)
 
     def load(self, filename):
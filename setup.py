c8c2ab3e274d543bf27381df87642413d43efb28
sergey-tomin <tomin.sergey@gmail.com>
2017-10-01 22:14:17 +0200

    setup file changed

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 from setuptools import setup, find_packages
 from os.path import join, dirname
 
+all_packages = []
+for pkg in find_packages():
+    if "demos" in pkg:
+        pkg = "ocelot." +  pkg
+    all_packages.append(pkg)
+
+
 setup(
     name='ocelot',
     version='17.10',
@@ -8,7 +15,8 @@ setup(
     author='ocelot-collab',
     author_email='tomin.sergey@gmail.com',
     url='https://github.com/ocelot-collab/ocelot',
-    packages=find_packages(),
+    packages=all_packages,
+    package_dir={'ocelot.demos': 'demos'},  ## install examples along with the rest of the source
     install_requires=[
         'numpy', 'scipy', 'matplotlib'
     ],

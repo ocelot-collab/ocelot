4f142d2697eb967a4be27e13e3004c0640977a8b
sergey-tomin <tomin.sergey@gmail.com>
2017-10-27 13:54:03 +0200

    added gui tools folder

---
+++ gui_tools/gui/gui_main.py
@@ -0,0 +1,164 @@
+from gui.UIOnline import *
+#from table_test import *
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QAction, QTableWidget, QTableWidgetItem, QVBoxLayout
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
+from PyQt5 import QtCore
+from gui.device.device_ui import *
+from ocelot import *
+import os
+
+class OcelotInterfaceWindow(QMainWindow):
+    """ Main class for the GUI application """
+    def __init__(self, master=None):
+        super().__init__()
+
+        # initialize
+        #QFrame.__init__(self)
+
+        #self.logbook = "xfellog"
+        #self.dev_mode = False
+        self.master = master
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.loadStyleSheet()
+        self.ui.gridLayout_6.setContentsMargins(0,0,0,0)
+
+        self.ui.w_q_table.ui.pb_calculate.clicked.connect(self.master.calc_twiss)
+
+        self.ui.pb_start_tracking.clicked.connect(self.ui_start_s2e)
+        self.ui.pb_hide_s2e_pan.clicked.connect(self.hide_show_s2e)
+        self.ui.pb_hide_cav_pan.clicked.connect(self.hide_show_cav)
+        self.ui.horizontalLayout.setContentsMargins(0,0,0,0)
+        self.ui.gridLayout_3.setContentsMargins(0,0,0,0)
+
+        self.ui.pb_twiss_from_track.clicked.connect(self.plot_track_twiss)
+
+        self.ui.pb_hide_show_quads.clicked.connect(self.hide_show_quads)
+        self.ui.pb_read.clicked.connect(self.master.read_from_doocs)
+        #self.stop_track_timer = QtCore.QTimer()
+        #self.stop_track_timer.timeout.connect(self.check_tread_is_alive)
+
+
+
+    def ui_start_s2e(self):
+        if self.ui.pb_start_tracking.text() == "Stop Tracking":
+            self.ui.pb_start_tracking.setText("Start Tracking")
+            self.ui.pb_start_tracking.setStyleSheet("color: rgb(85, 255, 127);")
+            self.master.stop_s2e()
+        else:
+            self.master.start_s2e()
+            self.ui.pb_start_tracking.setText("Stop Tracking")
+            self.ui.pb_start_tracking.setStyleSheet("color: rgb(255, 0, 0);")
+
+    def closeEvent(self, event):
+        self.ui.w_s2e.close()
+        QMainWindow.closeEvent(self, event)
+
+
+    def hide_show_quads(self):
+        if self.ui.pb_hide_show_quads.text() == "Hide Quads Panel":
+            self.ui.pb_hide_show_quads.setText("Show Quads Panel")
+            self.ui.pb_hide_show_quads.setStyleSheet("color: rgb(255, 0, 255);")
+            self.ui.w_q_table.hide()
+        else:
+            self.ui.w_q_table.show()
+            self.ui.pb_hide_show_quads.setText("Hide Quads Panel")
+            self.ui.pb_hide_show_quads.setStyleSheet("color: rgb(85, 255, 255);")
+
+    def hide_show_s2e(self):
+        if self.ui.pb_hide_s2e_pan.text() == "Hide S2E Panel":
+            self.ui.pb_hide_s2e_pan.setText("Show S2E Panel")
+            self.ui.pb_hide_s2e_pan.setStyleSheet("color: rgb(255, 0, 255);")
+            self.ui.w_s2e.hide()
+        else:
+            self.ui.w_s2e.show()
+            self.ui.pb_hide_s2e_pan.setText("Hide S2E Panel")
+            self.ui.pb_hide_s2e_pan.setStyleSheet("color: rgb(85, 255, 255);")
+
+    def hide_show_cav(self):
+        if self.ui.pb_hide_cav_pan.text() == "Hide Cavity Panel":
+            self.ui.pb_hide_cav_pan.setText("Show Cavity Panel")
+            self.ui.pb_hide_cav_pan.setStyleSheet("color: rgb(255, 0, 255);")
+            self.ui.w_cav_table.hide()
+        else:
+            self.ui.w_cav_table.show()
+            self.ui.pb_hide_cav_pan.setText("Hide Cavity Panel")
+            self.ui.pb_hide_cav_pan.setStyleSheet("color: rgb(85, 255, 255);")
+
+
+    #def set_s2e_calc_obj(self, calc_obj):
+    #    self.s2e_calc_obj = calc_obj
+    def load_all_twiss(self):
+        bx = []
+        by = []
+        s = []
+        E = []
+        for sec in self.master.sections:
+            print(sec.lattice_name, os.path.isfile(sec.tws_file) )#if sec.tws_file
+            if os.path.isfile(sec.tws_file):
+                tws_data = sec.load_twiss_file()
+            else:
+                print(sec.lattice_name, "NO TWISS FILE")
+
+            bx = np.append(bx, tws_data["beta_x"])
+            by = np.append(by, tws_data["beta_y"])
+            s0 = 0 if len(s) == 0 else s[-1]
+            s = np.append(s, tws_data["s"]+s0)
+            E = np.append(E, tws_data["E"])
+        return s, bx, by, E
+
+
+    def plot_track_twiss(self):
+        s, bx, by, E = self.load_all_twiss()
+        self.ui.w_twiss_monitor.update_plot_track(s, bx, by, E)
+
+    def plot_s2e(self, particles):
+        self.ui.w_s2e_monitor.plot_s2e(particles)
+
+    def init_quad_table(self, devs, calc_obj):
+        self.ui.w_q_table.init_table(devs=devs, device_iface=DeviceUI, calc_obj=calc_obj, spin_params=[-5000, 5000, 0.1])
+
+    def init_s2e_table(self, sections, check_header="", phys_proc_names=[]):
+        self.ui.w_s2e.init_table(sections=sections, check_header=check_header,
+                                 phys_proc_names=phys_proc_names)
+
+    def init_cavs_table(self, devs, calc_obj):
+        self.ui.w_cav_table.init_table(devs=devs, device_iface=CavityUI, calc_obj=calc_obj,
+                                       spin_params_phi=[-360, 360, 0.1],
+                                       spin_params_v=[0, 1000, 0.001])
+
+
+    def loadStyleSheet(self):
+        """ Load in the dark theme style sheet. """
+        try:
+            self.cssfile = "gui/style.css"
+            with open(self.cssfile, "r") as f:
+                self.setStyleSheet(f.read())
+        except IOError:
+            print('No style sheet found!')
+
+def main():
+
+    QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_X11InitThreads)
+
+    #create the application
+    app = QApplication(sys.argv)
+    # setting the path variable for icon
+    #path = os.path.join(os.path.dirname(sys.modules[__name__].__file__), 'ocelot.png')
+    #app.setWindowIcon(QtGui.QIcon(path))
+    window = OcelotInterfaceWindow()
+
+
+    #window.setWindowIcon(QtGui.QIcon('ocelot.png'))
+    window.show()
+
+    #Build documentaiton if source files have changed
+    # TODO: make more universal
+    #os.system("cd ./docs && xterm -T 'Ocelot Doc Builder' -e 'bash checkDocBuild.sh' &")
+    #exit script
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
---
+++ gui_tools/main.py
@@ -0,0 +1,296 @@
+import sys, os
+path = os.path.realpath(__file__)
+indx = path.find("gui_tools")
+print("PATH", os.path.realpath(__file__), path[:indx])
+sys.path.append(path[:indx])
+from ocelot import *
+from accelerator.s2e_sections import sections
+from gui_tools.gui.gui_main import OcelotInterfaceWindow
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QMainWindow, QApplication
+import time
+import numpy as np
+from copy import deepcopy
+from threading import Thread, Event
+from ocelot.optimizer.mint.xfel_interface import *
+from mint.devices_mi import *
+
+tws_i1 = Twiss()
+tws_i1.E = 0.005000000
+tws_i1.beta_x  = 55.7887190242
+tws_i1.beta_y  = 55.7887190242
+tws_i1.alpha_x = 18.185436973
+tws_i1.alpha_y = 18.185436973
+tws_i1.beta_x  = 0.286527307369
+tws_i1.beta_y  = 0.286527307369
+tws_i1.alpha_x = -0.838833736086
+tws_i1.alpha_y = -0.838833736086
+
+class BigCavity:
+    def __init__(self, eid):
+        self.id = eid
+        self.cavs = []
+
+    @property
+    def phi(self):
+        return np.mean([cav.phi for cav in self.cavs])
+
+    @phi.setter
+    def phi(self, value):
+        for cav in self.cavs:
+            cav.phi = value
+
+    @property
+    def v(self):
+        return np.sum([cav.v for cav in self.cavs])
+
+    @v.setter
+    def v(self, value):
+        cav_v = value/len(self.cavs)
+        for cav in self.cavs:
+            cav.v = cav_v
+
+class TrackTread(Thread):
+    def __init__(self, sections, phys_procs, check_header, gui):
+        super(TrackTread, self).__init__()
+        self.sections = sections
+        self.phys_procs = phys_procs
+        self.check_header = check_header
+        self.gui = gui
+        self.particles = None
+        self.kill_track = False
+
+    def kill_tracking(self):
+        for sec in self.sections:
+            sec.kill_track = True
+            if "navigator" in sec.__dict__.keys():
+                sec.navigator.kill_process = True
+
+    def start_tracking(self):
+        for sec in self.sections:
+            sec.kill_track = False
+            if "navigator" in sec.__dict__.keys():
+                sec.navigator.kill_process = False
+
+    def run(self):
+        particles = self.particles
+        t0 = time.time()
+        self.start_tracking()
+        for sec in self.sections:
+            if sec.kill_track:
+                continue
+            for proc in self.phys_procs:
+                flag = sec.ui.get_status(proc=proc.__name__)
+                flag_name = sec.translator[proc]
+                sec.__dict__[flag_name] = flag
+                #print(proc.__name__, flag_name, flag)
+            if sec.ui.get_status(proc=self.check_header):
+                #print("tracking trough "+sec.__class__.__name__ + " ....")
+                #sec.calc_tws = False
+                particles = sec.tracking(particles)
+                #sec.calculated = True
+                sec.ui.calculated(True)
+                self.gui.plot_s2e(particles)
+            print(sec.__class__.__name__ + "time exec: ", time.time() - t0)
+
+
+class S2ETool:
+    """ Main class for the GUI application """
+    def __init__(self):
+        self.gui = OcelotInterfaceWindow(master=self)
+        self.ui = self.gui.ui
+        self.tws0 = tws_i1
+        self.online_calc = True
+        self.phys_procs = [CSR, SpaceCharge, WakeKick, BeamTransform]
+        self.check_header = "Calculate?"
+        self.load_sections()
+        self.load_s2e_table()
+        self.load_quads()
+        self.load_cavities(cav_names=["A1", "AH1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10",
+                                      "A11", "A12", "A13", "A14", "A15", "A16", "A17","A18", "A19", "A20",
+                                      "A21", "A22", "A23", "A24", "A25"])
+
+        self.mi = XFELMachineInterface()
+        self.magnets = MIMagnets()
+        self.magnets.mi = self.mi
+        self.mi_cav_obj = MICavity()
+        self.mi_cav_obj.mi = self.mi
+        self.track_thread = None
+
+    def stop_s2e(self):
+        if self.track_thread != None:
+            self.track_thread.stop_flag = True
+            self.track_thread.kill_tracking()
+
+    def start_s2e(self):
+        self.update_lattice_from_tables()
+        particles = None
+        for sec in self.sections:
+            sec.ui.calculated(False)
+        self.track_thread = TrackTread(self.sections, self.phys_procs, self.check_header, self.gui)
+        self.track_thread.start()
+
+    def load_s2e_table(self):
+        all_phys_procs = [proc.__name__ for proc in self.phys_procs]
+
+        self.gui.init_s2e_table(self.sections, self.check_header, phys_proc_names=all_phys_procs)
+
+    def load_quads(self):
+        self.quads = []
+        for sec in self.sections:
+            for elem in sec.lattice.sequence:
+                if elem.__class__ == Quadrupole:
+                    self.quads.append(elem)
+
+        self.gui.init_quad_table(self.quads, calc_obj=self.calc_twiss)
+
+    def update_lat(self):
+        for sec in self.sections:
+            sec.lattice.update_transfer_maps()
+
+    def load_cavities(self, cav_names):
+        self.cavs = []
+        for cav_name in cav_names:
+            cav  = BigCavity(eid=cav_name)
+            self.cavs.append(cav)
+
+        for sec in self.sections:
+
+            for elem in sec.lattice.sequence:
+                if elem.__class__ == Cavity:
+                    for cav in self.cavs:
+                        if "." + cav.id + "." in elem.id:
+                            cav.cavs.append(elem)
+
+                    #self.cavs.append(elem)
+
+        self.gui.init_cavs_table(self.cavs, calc_obj=self.calc_twiss)
+
+    def calc_twiss(self, calc=True):
+        #lat = MagneticLattice(cell)
+        if self.online_calc == False:
+            return
+
+        # L = 0
+        tws0 = deepcopy(self.tws0)
+        tws_all = []
+        self.table2cavs()
+        self.table2quads()
+        for sec in self.sections:
+        #    for elem in sec.lattice.sequence:
+        #        if elem.__class__ in [Quadrupole]:
+        #            #print(elem.id, elem.row)
+        #            #elem.kick_mrad = elem.ui.get_value()
+        #            elem.k1 = elem.ui.get_value()
+        #            elem.ui.value_was_changed(np.abs(np.abs(elem.ui.get_init_value()) - np.abs(elem.k1))> 0.1)
+        #
+        #
+        #                #self.r_items[elem.ui.row].setBrush(pg.mkBrush("r"))
+        #                #self.ui.tableWidget.item(elem.row, 1).setForeground(QtGui.QColor(255, 101, 101))  # red
+        #            #else:
+        #                #self.ui.tableWidget.item(elem.row, 1).setForeground(QtGui.QColor(255, 255, 255))  # white
+        #                #self.r_items[elem.ui.row].setBrush(pg.mkBrush("g"))
+        #            #r = self.r_items[elem.ui.row]
+        #            #sizes = r.init_params
+        #            #sizes = list(r.boundingRect().getRect())
+        #            #sizes[3] = 10*elem.kick_mrad/self.quad_ampl
+        #            #r.setRect(sizes[0], sizes[1], sizes[2], sizes[3])
+        #
+            sec.lattice.update_transfer_maps()
+            tws = twiss(sec.lattice, tws0)
+            tws0 = tws[-1]
+            tws_all = np.append(tws_all, tws)
+
+        beta_x = [tw.beta_x for tw in tws_all]
+        beta_y = [tw.beta_y for tw in tws_all]
+        dx = [tw.Dx for tw in tws_all]
+        dy = [tw.Dy for tw in tws_all]
+        s = [tw.s for tw in tws_all]
+
+        self.ui.w_twiss_monitor.update_plot(s, beta_x, beta_y, dx, dy)
+
+    def table2quads(self):
+        for elem in self.quads:
+            k1 = elem.ui.get_value()
+            elem.k1 = k1
+            elem.ui.value_was_changed(np.abs(np.abs(elem.ui.get_init_value() - k1)) > 0.1)
+
+    def table2cavs(self):
+        for elem in self.cavs:
+            v = elem.ui.get_volt()
+            phi = elem.ui.get_phi()
+            elem.v = v
+            elem.phi = phi
+            elem.ui.phi_was_changed(np.abs(np.abs(elem.ui.get_init_phi() - phi)) > 0.01)
+            elem.ui.volt_was_changed(np.abs(np.abs(elem.ui.get_init_volt() - v)) > 0.01)
+            #print(elem.id , v, phi)
+            #elem.transfer_map = self.lat.method.create_tm(elem)
+
+    def update_lattice_from_tables(self):
+        self.table2quads()
+        self.table2cavs()
+        self.update_lat()
+
+    def read_from_doocs(self):
+        self.online_calc = False
+        self.magnets.get_magnets(self.quads)
+        self.mi_cav_obj.get_cavities(self.cavs)
+        self.update_lattice_from_tables()
+        self.online_calc = True
+        self.calc_twiss()
+        print("Calculate")
+
+    def init_lattice(self):
+        pass
+
+    #def calc_twiss(self):
+    #    pass
+
+    def part_tracking(self):
+        pass
+
+    def load_particles(self):
+        pass
+
+
+    def load_sections(self):
+        blank_sections = sections.sections
+        self.sections = []
+        for sec in blank_sections:
+            self.sections.append(sec())
+        #for sec in self.sections:
+        #    print(sec.__class__.__name__)
+        return self.sections
+
+    def get_whole_lat(self):
+        pass
+
+
+
+
+
+def main():
+
+    QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_X11InitThreads)
+
+    #create the application
+    app = QApplication(sys.argv)
+    # setting the path variable for icon
+    #path = os.path.join(os.path.dirname(sys.modules[__name__].__file__), 'ocelot.png')
+    #app.setWindowIcon(QtGui.QIcon(path))
+    window = S2ETool()
+
+
+    #window.setWindowIcon(QtGui.QIcon('ocelot.png'))
+    window.gui.show()
+
+    #Build documentaiton if source files have changed
+    # TODO: make more universal
+    #os.system("cd ./docs && xterm -T 'Ocelot Doc Builder' -e 'bash checkDocBuild.sh' &")
+    #exit script
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()

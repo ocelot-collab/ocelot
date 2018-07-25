96b65f0fd56e5cd8aabd237c9a42d02da6e62de4
sergey-tomin <tomin.sergey@gmail.com>
2018-07-25 10:21:54 +0200

    added new unit tests

--- demos_tests/ebeam_test/csr_ex/csr_ex_test.py
+++ demos_tests/ebeam_test/csr_ex/csr_ex_test.py
@@ -34,7 +34,7 @@ def test_lattice_transfer_map_RT(lattice, p_array, parameter, update_ref_values=
 
     if parameter == 1:
         for elem in lattice.sequence:
-            if elem == Bend:
+            if elem.__class__ == Bend:
                 elem.tilt = np.pi / 2
 
         lattice.update_transfer_maps()
@@ -45,12 +45,32 @@ def test_lattice_transfer_map_RT(lattice, p_array, parameter, update_ref_values=
 
     assert check_result([result1, result2])
 
-def test_track_without_csr(lattice, p_array, parameter=None, update_ref_values=False):
-    """track function test without CSR"""
-    
+
+@pytest.mark.parametrize('parameter', [0, 1])
+def test_track_without_csr(lattice, p_array, parameter, update_ref_values=False):
+    """
+    track function test without CSR
+
+    0 - normal tracking
+    1 - tilt bending magnets and tilt back electron beam then untilt beam and compare with ref beam (twiss not checked)
+    """
+
+    tilt = 0
+    if parameter == 1:
+        tilt = np.pi/2.
+
+    for elem in lattice.sequence:
+        if elem.__class__ == Bend:
+            elem.tilt = tilt
+
+    lattice.update_transfer_maps()
+
     navi = Navigator(lattice)
     navi.unit_step = 0.05
+
+    p_array.rparticles[:] = np.dot(rot_mtx(-tilt), p_array.rparticles)[:]
     pytest.tws_track_wo, pytest.p_array_wo = track(lattice, p_array, navi)
+    p_array.rparticles[:] = np.dot(rot_mtx(tilt), p_array.rparticles)[:]
 
     pytest.istracked_wo = True
 
@@ -62,29 +82,31 @@ def test_track_without_csr(lattice, p_array, parameter=None, update_ref_values=F
 
     tws_track_p_array_ref = json_read(REF_RES_DIR + sys._getframe().f_code.co_name + '.json')
 
+
     result1 = check_dict(tws_track, tws_track_p_array_ref['tws_track'], TOL, assert_info=' tws_track - ')
-    result2 = check_dict(p, tws_track_p_array_ref['p_array'], TOL, assert_info=' p - ')
+    if parameter == 1:
+        result1 = [None]
+    result2 = check_dict(p, tws_track_p_array_ref['p_array'], tolerance=TOL, assert_info=' p - ')
     assert check_result(result1+result2)
 
 
 def test_track_without_csr_tilted(lattice, p_array, parameter=None, update_ref_values=False):
     """track function test without CSR in vertical plane """
     lattice = copy.deepcopy(lattice)
-    navi = Navigator(lattice)
-    navi.unit_step = 0.05
+
 
     for elem in lattice.sequence:
-        if elem == Bend:
+        if elem.__class__ == Bend:
             elem.tilt = np.pi / 2
 
     lattice.update_transfer_maps()
+    navi = Navigator(lattice)
+    navi.unit_step = 0.05
+    tws_track_wo, p_array_wo = track(lattice, p_array, navi)
 
-    pytest.tws_track_wo, pytest.p_array_wo = track(lattice, p_array, navi)
 
-    pytest.istracked_wo = True
-
-    tws_track = obj2dict(pytest.tws_track_wo)
-    p = obj2dict(pytest.p_array_wo)
+    tws_track = obj2dict(tws_track_wo)
+    p = obj2dict(p_array_wo)
 
     if update_ref_values:
         return {'tws_track': tws_track, 'p_array': p}
@@ -100,8 +122,10 @@ def test_track_without_csr_rotated(lattice, p_array, parameter=None, update_ref_
     """track function test without CSR, comparison between the rotated beam and track in vertical plane"""
     lattice_copy = copy.deepcopy(lattice)
     p_array_copy = copy.deepcopy(p_array)
+
     navi = Navigator(lattice_copy)
     navi.unit_step = 0.05
+
     tilt = np.pi / 2
     for elem in lattice_copy.sequence:
         if elem.__class__ == Bend:
@@ -109,13 +133,22 @@ def test_track_without_csr_rotated(lattice, p_array, parameter=None, update_ref_
 
     lattice_copy.update_transfer_maps()
 
+
+
     tws_track_tilted, p_array_wo_tilted = track(lattice_copy, p_array_copy, navi)
 
-    p_array.rparticles[:] = np.dot(rot_mtx(tilt), p_array.rparticles)[:]
+    tilt = np.pi / 2
+    for elem in lattice.sequence:
+        if elem.__class__ == Bend:
+            elem.tilt = 0
+
+    lattice.update_transfer_maps()
+
     navi = Navigator(lattice)
     navi.unit_step = 0.05
-    tws_track_rot, p_array = track(lattice, p_array, navi)
 
+    p_array.rparticles[:] = np.dot(rot_mtx(tilt), p_array.rparticles)[:]
+    tws_track_rot, p_array = track(lattice, p_array, navi)
     p_array.rparticles[:] = np.dot(rot_mtx(-tilt), p_array.rparticles)[:]
 
     p1 = obj2dict(p_array_wo_tilted)
@@ -124,8 +157,25 @@ def test_track_without_csr_rotated(lattice, p_array, parameter=None, update_ref_
     result2 = check_dict(p1, p2, TOL, assert_info=' p - ')
     assert check_result(result2)
 
-def test_track_with_csr(lattice, p_array, parameter=None, update_ref_values=False):
-    """track function test with CSR"""
+
+@pytest.mark.parametrize('parameter', [0, 1])
+def test_track_with_csr(lattice, p_array, parameter, update_ref_values=False):
+    """
+    track function test with CSR
+
+    0 - normal tracking
+    1 - tilt bending magnets and tilt back electron beam then untilt beam and compare with ref beam (twiss not checked)
+    """
+
+    tilt = 0
+    if parameter == 1:
+        tilt = np.pi/2.
+
+    for elem in lattice.sequence:
+        if elem.__class__ == Bend:
+            elem.tilt = tilt
+
+    lattice.update_transfer_maps()
     
     csr = CSR()
     csr.traj_step = 0.0002
@@ -135,7 +185,9 @@ def test_track_with_csr(lattice, p_array, parameter=None, update_ref_values=Fals
     navi.add_physics_proc(csr, lattice.sequence[0], lattice.sequence[-1])
     navi.unit_step = 0.05
 
+    p_array.rparticles[:] = np.dot(rot_mtx(-tilt), p_array.rparticles)[:]
     pytest.tws_track_w, pytest.p_array_w = track(lattice, p_array, navi)
+    p_array.rparticles[:] = np.dot(rot_mtx(tilt), p_array.rparticles)[:]
 
     pytest.istracked_w = True
 
@@ -148,41 +200,11 @@ def test_track_with_csr(lattice, p_array, parameter=None, update_ref_values=Fals
     tws_track_p_array_ref = json_read(REF_RES_DIR + sys._getframe().f_code.co_name + '.json')
 
     result1 = check_dict(tws_track, tws_track_p_array_ref['tws_track'], TOL, assert_info=' tws_track - ')
+    if parameter == 1:
+        result1 = [None]
     result2 = check_dict(p, tws_track_p_array_ref['p_array'], TOL, assert_info=' p_array - ')
     assert check_result(result1+result2)
 
-
-def test_track_with_csr_tilted(lattice, p_array, parameter=None, update_ref_values=False):
-    """track function test with CSR"""
-    lattice = copy.deepcopy(lattice)
-
-    csr = CSR()
-    csr.traj_step = 0.0002
-    csr.apply_step = 0.0005
-
-    navi = Navigator(lattice)
-    navi.add_physics_proc(csr, lattice.sequence[0], lattice.sequence[-1])
-    navi.unit_step = 0.05
-    for elem in lattice.sequence:
-        if elem == Bend:
-            elem.tilt = np.pi / 2
-
-    lattice.update_transfer_maps()
-    pytest.tws_track_w, pytest.p_array_w = track(lattice, p_array, navi)
-
-    pytest.istracked_w = True
-
-    tws_track = obj2dict(pytest.tws_track_w)
-    p = obj2dict(pytest.p_array_w)
-
-    if update_ref_values:
-        return {'tws_track': tws_track, 'p_array': p}
-
-    tws_track_p_array_ref = json_read(REF_RES_DIR + sys._getframe().f_code.co_name + '.json')
-
-    result1 = check_dict(tws_track, tws_track_p_array_ref['tws_track'], TOL, assert_info=' tws_track - ')
-    result2 = check_dict(p, tws_track_p_array_ref['p_array'], TOL, assert_info=' p_array - ')
-    assert check_result(result1 + result2)
 
 @pytest.mark.parametrize('parameter', [0, 1])
 def test_get_current(lattice, p_array, parameter, update_ref_values=False):
@@ -193,12 +215,12 @@ def test_get_current(lattice, p_array, parameter, update_ref_values=False):
 
     if parameter == 0:
         if not hasattr(pytest, 'istracked_wo') or not pytest.istracked_wo:
-            test_track_without_csr(lattice, p_array, None, True)
+            test_track_without_csr(lattice, p_array, 0, True)
 
         p = pytest.p_array_wo        
     else:
         if not hasattr(pytest, 'istracked_w') or not pytest.istracked_w:
-            test_track_with_csr(lattice, p_array, None, True)
+            test_track_with_csr(lattice, p_array, 0, True)
         
         p = pytest.p_array_w
     
@@ -251,7 +273,6 @@ def test_update_ref_values(lattice, p_array, cmdopt):
     update_functions.append('test_track_without_csr')
     update_functions.append('test_track_without_csr_tilted')
     update_functions.append('test_track_with_csr')
-    update_functions.append('test_track_with_csr_tilted')
 
     update_functions.append('test_get_current')
     

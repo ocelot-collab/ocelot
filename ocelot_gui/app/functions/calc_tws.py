ec56023c589221eda882b9ee021f12cf9a8e392d
Yevgeniy Fomin <yafomin@gmail.com>
2018-11-30 12:14:21 +0300

    improvements and updates in GUI

---
+++ ocelot_gui/app/functions/calc_tws.py
@@ -0,0 +1,38 @@
+"""Calculation twiss parameters description"""
+
+from ocelot.cpbd.optics import *
+
+
+class TwissParameters():
+
+    def __init__(self, lattice):
+        
+        self.lattice = lattice
+        self.tws = None
+        self.tws_step = 0.0
+
+
+    def __del__(self):
+        pass
+
+
+    def calc_twiss(self):
+
+        if self.lattice.periodic_solution:
+            tws0 = periodic_twiss(Twiss(), lattice_transfer_map(self.lattice.lattice, self.lattice.tws0.E))
+        else:
+            tws0 = self.lattice.tws0
+
+        if tws0 is not None:
+
+            # fix only for periodic solution
+            tws0.E = self.lattice.tws0.E
+
+            if self.tws_step != 0.0:
+                n = int(self.lattice.lattice.totalLen / self.tws_step) + 1
+            else:
+                n = None
+
+            self.tws = twiss(self.lattice.lattice, tws0=tws0, nPoints=n)
+        else:
+            self.tws = None
--- ocelot_gui/app/lattice.py
+++ ocelot_gui/app/lattice.py
@@ -16,15 +16,46 @@ class GUILattice():
         self.nsuperperiods = 1
         
         self.method = MethodTM()
+        
+        # use for first order matrix tracking
         self.method.global_method = TransferMap
         self.method.params[Sextupole] = KickTM
+        
+        # use for second order matrix tracking
         #self.method.global_method = SecondTM
 
         self.lattice = None
         self.periodic_solution = False
 
+        self.tunable_elements = {'Bend':['angle', 'k1'],'SBend':['angle', 'k1'], 'RBend':['angle', 'k1'], 'Quadrupole':['k1'], 'Drift':['l']}
+        self.matchable_elements = {'Bend':'k1','SBend':'k1', 'RBend':'k1', 'Quadrupole':'k1', 'Drift':'l'}
+
 
     def init_lattice(self):
         """Create magnetic lattice from the last cell sequences"""
         
+        self.init_tuneablity()
+        self.init_matchablity()
         self.lattice = MagneticLattice(self.cell,  method=self.method)
+
+
+    def init_tuneablity(self):
+        """Set possibility to tune element"""
+
+        for elem_id, elem in self.elements.items():
+            if elem.__class__.__name__ in self.tunable_elements:
+                elem.is_tuneable = True
+                elem.tune_params = self.tunable_elements[elem.__class__.__name__]
+            else:
+                elem.is_tuneable = False
+
+
+    def init_matchablity(self):
+        """Set possibility to match element"""
+
+        for elem_id, elem in self.elements.items():
+            if elem.__class__.__name__ in self.matchable_elements:
+                elem.is_matchable = True
+                elem.match_params = self.matchable_elements[elem.__class__.__name__]
+            else:
+                elem.is_matchable = False

013a4bd010676064e9aa37a110006a4db6907c6a
sergey-tomin <tomin.sergey@gmail.com>
2019-07-29 16:37:48 +0200

    updated tutorials

--- ocelot/gui/accelerator.py
+++ ocelot/gui/accelerator.py
@@ -471,10 +471,8 @@ def plot_disp(ax, tws, top_plot, font_size):
         greek = ""
         if "beta" in elem or "alpha" in elem or "mu" in elem:
             greek = "\\"
-        if "mux" == elem:
-            elem = elem.replace("x", "_x")
-        if "muy" == elem:
-            elem = elem.replace("y", "_y")
+        if "mu" in elem:
+            elem = elem.replace("mu", "mu_")
         top_label = r"$" + greek + elem+"$"
         ax.plot(S, Ftop, lw = 2, label=top_label)
         d_Ftop.append( max(Ftop) - min(Ftop))

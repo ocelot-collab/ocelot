341f70f940e76e6eecd05f7f5673fe83ccfbd191
sergey-tomin <tomin.sergey@gmail.com>
2020-05-13 17:27:16 +0200

    updated version number. Required a minor release.

--- ocelot/__init__.py
+++ ocelot/__init__.py
@@ -2,7 +2,7 @@
 general ocelot description
 """
 
-__version__ = '20.05.0'
+__version__ = '20.05.1'
 
 
 __all__ = ['Twiss', "Beam", "Particle", "get_current", "get_envelope", "generate_parray",           # beam

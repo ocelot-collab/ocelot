f697716d91825d29cbd34deeef14bb742bdd507e
michael-boese <michael.boese@desy.de>
2020-11-25 12:11:26 +0100

    star import for transformations

--- ocelot/__init__.py
+++ ocelot/__init__.py
@@ -43,20 +43,7 @@ import numpy as np
 from ocelot.cpbd.magnetic_lattice import MagneticLattice, merger
 
 from ocelot.cpbd.beam import *
-from ocelot.cpbd.transformations.cavity import CavityTM
-from ocelot.cpbd.transformations.corrector import CorrectorTM
-from ocelot.cpbd.transformations.coupler_kick import CouplerKickTM
-from ocelot.cpbd.transformations.first_order import TransferMap
-from ocelot.cpbd.transformations.h_corrector import HCorrectorTM
-from ocelot.cpbd.transformations.kick import KickTM
-from ocelot.cpbd.transformations.multipole import MultipoleTM
-from ocelot.cpbd.transformations.pulse import PulseTM
-from ocelot.cpbd.transformations.runge_kutta import RungeKuttaTM
-from ocelot.cpbd.transformations.runge_kutta_tr import RungeKuttaTrTM
-from ocelot.cpbd.transformations.second_order import SecondTM
-from ocelot.cpbd.transformations.tw_cavity import TWCavityTM
-from ocelot.cpbd.transformations.undulator_test import UndulatorTestTM
-from ocelot.cpbd.transformations.v_corrector import VCorrectorTM
+from ocelot.cpbd.transformations import *
 from ocelot.cpbd.elements import *
 from ocelot.cpbd.match import *
 from ocelot.cpbd.track import *
--- ocelot/cpbd/transformations/__init__.py
+++ ocelot/cpbd/transformations/__init__.py
@@ -1,1 +1,17 @@
+__all__ = ['CavityTM', 'CorrectorTM', 'CouplerKickTM', 'TransferMap', 'HCorrectorTM', 'KickTM', 'MultipoleTM',
+           'PulseTM', 'RungeKuttaTM', 'RungeKuttaTrTM', 'SecondTM', 'TWCavityTM', 'UndulatorTestTM', 'VCorrectorTM']
 
+from ocelot.cpbd.transformations.cavity import CavityTM
+from ocelot.cpbd.transformations.corrector import CorrectorTM
+from ocelot.cpbd.transformations.coupler_kick import CouplerKickTM
+from ocelot.cpbd.transformations.first_order import TransferMap
+from ocelot.cpbd.transformations.h_corrector import HCorrectorTM
+from ocelot.cpbd.transformations.kick import KickTM
+from ocelot.cpbd.transformations.multipole import MultipoleTM
+from ocelot.cpbd.transformations.pulse import PulseTM
+from ocelot.cpbd.transformations.runge_kutta import RungeKuttaTM
+from ocelot.cpbd.transformations.runge_kutta_tr import RungeKuttaTrTM
+from ocelot.cpbd.transformations.second_order import SecondTM
+from ocelot.cpbd.transformations.tw_cavity import TWCavityTM
+from ocelot.cpbd.transformations.undulator_test import UndulatorTestTM
+from ocelot.cpbd.transformations.v_corrector import VCorrectorTM

0b6045f3a87a65321d109930c3502f52b61355a7
Andrei <trebandrej@gmail.com>
2023-04-06 12:11:49 +0200

    end_poles moved to Undulator

--- ocelot/cpbd/elements/undulator.py
+++ ocelot/cpbd/elements/undulator.py
@@ -4,8 +4,8 @@ from ocelot.cpbd.transformations.transfer_map import TransferMap
 
 
 class Undulator(OpticElement):
-    def __init__(self, lperiod=0., nperiods=0, Kx=0., Ky=0., field_file=None, eid=None, tm=TransferMap, **params):
-        super().__init__(UndulatorAtom(lperiod=lperiod, nperiods=nperiods, Kx=Kx, Ky=Ky, field_file=field_file, eid=eid),
+    def __init__(self, lperiod=0., nperiods=0, Kx=0., Ky=0., phase=0, end_poles='1', field_file=None, eid=None, tm=TransferMap, **params):
+        super().__init__(UndulatorAtom(lperiod=lperiod, nperiods=nperiods, Kx=Kx, Ky=Ky, phase=phase, end_poles=end_poles, field_file=field_file, eid=eid),
                          tm=tm, default_tm=TransferMap, **params)
 
     

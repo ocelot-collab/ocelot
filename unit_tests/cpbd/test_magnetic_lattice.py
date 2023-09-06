ba9a3a6181fb565bb9d8f4ff8b028319d1fadb29
Stuart Walker <stuart.derek.walker@gmail.com>
2023-09-06 11:48:26 +0300

    insert marker by predicate test

--- unit_tests/cpbd/test_magnetic_lattice.py
+++ unit_tests/cpbd/test_magnetic_lattice.py
@@ -1,8 +1,8 @@
 import pytest
 
 
-from ocelot.cpbd.magnetic_lattice import MagneticLattice
-from ocelot.cpbd.elements import Drift, SBend
+from ocelot.cpbd.magnetic_lattice import MagneticLattice, insert_markers_by_predicate
+from ocelot.cpbd.elements import Drift, SBend, Marker
 
 
 def test_magnetic_lattice_find_indices():
@@ -27,3 +27,32 @@ def test_magnetic_lattice_find_indices_by_predicate():
 
     indices = mlat.find_indices_by_predicate(pred)
     assert indices == [1, 3]
+
+
+def test_magnetic_lattice_insert_markers_by_predicate():
+    d1 = Drift(l=0.1)
+    sb = SBend(l=0.2, angle=3)
+    d2 = Drift(l=0.2)
+
+    sequence = [d1, sb, d2, sb]
+    mlat = MagneticLattice(sequence)
+    def pred(element):
+        return element.l == 0.2 and isinstance(element, SBend)
+
+    before_suffix = "before-suffix"
+    after_suffix = "after-suffix"    
+    insert_markers_by_predicate(sequence, pred,
+                                before_suffix=before_suffix,
+                                after_suffix=after_suffix)
+    isb = sequence.index(sb)
+    before = sequence[isb - 1]
+    after = sequence[isb + 1]    
+
+
+
+    assert isinstance(before, Marker)
+    assert isinstance(after, Marker)    
+    assert before.id == f"{sb.id}{before_suffix}"
+    assert after.id == f"{sb.id}{after_suffix}"    
+
+    

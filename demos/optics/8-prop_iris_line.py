c036560890dd1f895518a5916678eed220808c47
Andrei Trebushinin <trebandrej@gmail.com>
2023-09-29 15:31:07 +0200

    fixes pull request #195

--- demos/optics/8-prop_iris_line.py
+++ demos/optics/8-prop_iris_line.py
@@ -24,7 +24,7 @@ from copy import deepcopy
 
 def generate_center(N, 
                     sigma_x=2e-3, sigma_y=2e-3, 
-                    harmonics_coeffs=[(1.5, 5e-3, 0), (50, 0, 23)], 
+                    harmonics_coeffs_x=[(1.5, 5e-3, 0), (50, 0, 23)], 
                     harmonics_coeffs_y=[(1.5, -5e-3, 77), (50, -5e-3, 55)]):
     """
     Generate center_x and center_y based on parameters, the center coordinates for each iris line aperture  
@@ -34,7 +34,7 @@ def generate_center(N,
     :param sigma_x: Sigma for the random contribution in x.
     :param sigma_y: Sigma for the random contribution in y.
     :param harmonics_coeffs: List of tuples for harmonics in x.
-        Each tuple contains (frequency, coefficient, phase shift in degrees)
+        Each tuple contains (frequency, amplitude, phase shift in degrees)
     :param harmonics_coeffs_y: Similar to harmonics_coeffs but for y.
     :return: center_x, center_y
     """
@@ -46,7 +46,7 @@ def generate_center(N,
     rad_range = np.linspace(-np.pi, np.pi, N)
     
     # Harmonic contributions for x and y
-    x = sum([coeff*np.cos(rad_range*freq + np.deg2rad(phase)) for freq, coeff, phase in harmonics_coeffs])
+    x = sum([coeff*np.cos(rad_range*freq + np.deg2rad(phase)) for freq, coeff, phase in harmonics_coeffs_x])
     y = sum([coeff*np.cos(rad_range*freq + np.deg2rad(phase)) for freq, coeff, phase in harmonics_coeffs_y])
     
     # Final center_x and center_y
@@ -55,30 +55,30 @@ def generate_center(N,
     
     return center_x, center_y
 
-def plot_iris_line_prop_result(E_x, E_y, dfl_iris_exit, a, b, N, n_iter, rad_left_array, loss_per_cell_array, 
+def plot_iris_line_prop_result(E_x_lineout, E_y_lineout, dfl_iris_exit, a, b, N, n_iter_per_iris, rad_left_array, loss_per_cell_array,
                                direction = 'x', savefig=False, filePath='', loss_per_cell_max = 0.1):
 
     fontsize=18
     avr_cells = 500
     direction = direction
     
-    Z = np.linspace(0, N*b, n_iter)
+    Z = np.linspace(0, N*b, n_iter_per_iris*N)
     fig, axs = plt.subplots(3, 2, figsize=(15,8), gridspec_kw={'width_ratios': [4, 1]})
     fig.subplots_adjust(top=0.95, bottom=0.2, left=0.09, right=0.95)  # adjust this value to fit your needs
     
     cells = np.arange(0, N-1)
     
-    if n_iter/N == 1:
+    if n_iter_per_iris/N == 1:
         rad_left_array_ext = rad_left_array
         rad_left_array_ext = np.append(rad_left_array_ext, rad_left_array[-1])
     else:
-        rad_left_array_ext = np.repeat(rad_left_array, (n_iter/N))#.tolist()
+        rad_left_array_ext = np.repeat(rad_left_array, (n_iter_per_iris/N))#.tolist()
     
     if direction=='x':
-        I_2D = np.real(E_x)**2 + np.imag(E_x)**2
+        I_2D = np.real(E_x_lineout)**2 + np.imag(E_x_lineout)**2
         r = dfl_iris_exit.scale_x()*1e3
     elif direction=='y':
-        I_y_array1 = np.real(E_y)**2 + np.imag(E_y)**2
+        I_y_array1 = np.real(E_y_lineout)**2 + np.imag(E_y_lineout)**2
         r = dfl_iris_exit.scale_y()*1e3
     
         
@@ -224,16 +224,16 @@ Nf = a**2/dfl.xlamds/b
 print('Nf =', Nf)
 
 N = 2
-n_iter = 300
+n_iter_per_iris = 100
 dfl_iris_exit, E_x, E_y, rad_left_array, loss_per_cell_array = dfl_prop_iris(dfl, N=N, a=a, center=([0e-3, 0e-3], [0e-3, 0e-3]), 
-                                                                     b=b, n_iter=n_iter, absorption_outer_pipe=0,
+                                                                     b=b, n_iter_per_iris=n_iter_per_iris, absorption_outer_pipe=0,
                                                                      acount_first_cell_loss=0)
 
 #%% I.c Plot results
 # plot propagation results through these two cells
 I_x_array = np.real(E_x)**2 + np.imag(E_x)**2
 r = dfl_iris_exit.scale_y()*1e3
-Z = np.linspace(0, N*b, n_iter)
+Z = np.linspace(0, N*b, N*n_iter_per_iris)
 
 # Set the font size for axes labels and tick labels
 fontsize = 18
@@ -274,13 +274,12 @@ THz = 3
 xlamds = speed_of_light/THz/1e12
 
 N = 3000 ## we would like to study which mode tends to survive, this is 900m propagation of THz radiation
-n_iter = N
 b = 0.3
 a = 0.055
 
 
 sigma_g = 666
-dfl = generate_gaussian_dfl(xlamds=xlamds, shape=(351, 351, 1), dgrid=(4*a, 4*a,  10e-6), power_rms=(sigma_g * 1e-2/2, sigma_g*1e-2/2,  10e-6),
+dfl = generate_gaussian_dfl(xlamds=xlamds, shape=(151, 151, 1), dgrid=(4*a, 4*a,  10), power_rms=(sigma_g * 1e-2/2, sigma_g*1e-2/2,  1),
                           power_center=(0, 0, None), power_angle=(0, 0), power_waistpos=(0, 0), wavelength=None,
                           zsep=None, freq_chirp=0, en_pulse=None, power=1e6)
 Nf = a**2/dfl.xlamds/b
@@ -293,16 +292,15 @@ dfl_iris_exit = deepcopy(dfl)
 
 #%% II.1a Propagation through 3k cells
 
-dfl_iris_exit, E_x1, E_y1, rad_left_array1, loss_per_cell_array1 = dfl_prop_iris(dfl, N=N, a=a, b=b, 
-                                                                                 n_iter=n_iter, acount_first_cell_loss=1)
+dfl_iris_exit, E_x_lineout1, E_y_lineout1, rad_left_array1, loss_per_cell_array1 = dfl_prop_iris(dfl, N=N, a=a, b=b,
+                                                                                                 acount_first_cell_loss=1)
 #%% II.1b Plot results
-plot_iris_line_prop_result(E_x1, E_y1, dfl_iris_exit, a, b, N, n_iter, rad_left_array1, loss_per_cell_array1, 
-                               direction = 'x')
+plot_iris_line_prop_result(E_x_lineout1, E_y_lineout1, dfl_iris_exit, a, b, N, 1, 
+                           rad_left_array1, loss_per_cell_array1, direction = 'x')
 
 #%% II.2 Fundamental mode propagation 
 # now study of the survived more propagation over 300m of propagation
 N = 1000
-n_iter = N
 b = 0.3
 a = 0.055
 
@@ -312,11 +310,11 @@ rad_left_array2 = 0
 loss_per_cell_array2 = 0
 
 #%% II.2a Propagation through 300 cells
-dfl_waveguide2, E_x2, E_y2, rad_left_array2, loss_per_cell_array2 = dfl_prop_iris(dfl_iris_exit, N=N, a=a, b=b, 
-                                                                              n_iter=n_iter, acount_first_cell_loss=1)
+dfl_waveguide2, E_x_lineout2, E_y_lineout2, rad_left_array2, loss_per_cell_array2 = dfl_prop_iris(dfl_iris_exit, N=N, a=a, b=b,
+                                                                                                  acount_first_cell_loss=1)
 
 #%% II.2b Plot results
-plot_iris_line_prop_result(E_x2, E_y2, dfl_waveguide2, a, b, N, n_iter, rad_left_array2, loss_per_cell_array2, 
+plot_iris_line_prop_result(E_x_lineout2, E_y_lineout2, dfl_waveguide2, a, b, N, 1, rad_left_array2, loss_per_cell_array2, 
                                direction = 'x')
 
 #%% II.2c Comparison with analytics
@@ -357,7 +355,6 @@ THz = 3
 xlamds = speed_of_light/THz/1e12
 
 N = 1000
-n_iter = N
 b = 0.3
 a = 0.055
 
@@ -389,13 +386,13 @@ plt.xlim(0, len(center_x))
 plt.tight_layout()
 plt.show()
 #%% III.b Propagation
-dfl_iris_exit, E_x1, E_y1, rad_left_array1, loss_per_cell_array1 = dfl_prop_iris(dfl, N=N, a=a, b=b, center=(center_x, center_y),
-                                                                                 n_iter=n_iter, acount_first_cell_loss=1)
+dfl_iris_exit, E_x_lineout1, E_y_lineout1, rad_left_array1, loss_per_cell_array1 = dfl_prop_iris(dfl, N=N, a=a, b=b, center=(center_x, center_y),
+                                                                                                 acount_first_cell_loss=1)
 #%% III.b Plot results
-plot_iris_line_prop_result(E_x1, E_y1, dfl_iris_exit, a, b, N, n_iter, rad_left_array1, loss_per_cell_array1, 
+plot_iris_line_prop_result(E_x_lineout1, E_y_lineout1, dfl_iris_exit, a, b, N, 1, rad_left_array1, loss_per_cell_array1, 
                                direction = 'x', loss_per_cell_max=0.5)
 
-
+plot_dfl(dfl_iris_exit)
 
 
 

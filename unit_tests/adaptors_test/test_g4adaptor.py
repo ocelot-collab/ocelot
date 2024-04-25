469b36497e9baeca8f023a3031730b84a5991f4a
Christoph Lechner <christoph.lechner@xfel.eu>
2024-04-25 20:54:55 +0200

    genesis 1.3 v4 adaptor: added test for verbatim string -> lat file

--- unit_tests/adaptors_test/test_g4adaptor.py
+++ unit_tests/adaptors_test/test_g4adaptor.py
@@ -7,9 +7,6 @@ from ocelot.adaptors.genesis4 import *
 from ocelot.gui.beam_plot import *
 from ocelot.utils.xfel_utils import create_fel_beamline
 
-# TODO: Add test for functionality to write user-provided
-# string to lattice file. Can be done once the branches are merged.
-
 # Simple callback function for testing callbacks from 'write_gen4_lat'
 def my_cb_latline(lat, element, eleidx, element_prefix):
     # put marker for this test
@@ -61,3 +58,50 @@ def test_g4adaptor_writelat_cb(tmpdir):
 
     # check for presence of marker put in callback function
     assert sase_lat.sequence[0].cb_invoked==True
+
+###
+
+@pytest.mark.filterwarnings('ignore::PendingDeprecationWarning')
+def test_g4adaptor_writelat_verbstr(tmpdir):
+    '''
+    Code is based on test_g4adaptor_writelat_cb
+    '''
+
+    '''
+    Shorter lattice with SASE1-type FODO (uses functionality added by CL to OCELOT in Feb 2024)
+    New feature added in March-2024: Request complete final half-cell
+    -> this allows to match two-undulator-cell lattice
+    '''
+    sase_lat_pkg = create_fel_beamline('sase1', override_und_N=2, final_halfcell_complete=True)
+
+    # set up electron optics, undulator aw value
+    betaavg = 20 # m
+    Ephot = 9000 # eV
+    beam = generate_beam(
+        E=14, dE=0.003,
+        I=5000, shape='flattop', l_beam=1e-6, l_window=1.5e-6,
+        emit_n=0.4e-6, beta=betaavg, nslice=2)
+    prepare_el_optics(beam, sase_lat_pkg, E_photon=Ephot, beta_av=betaavg)
+
+    sase_lat, _, _ = sase_lat_pkg
+    #print(str(sase_lat))
+
+    # Test string (provided with trailing '\n' to write_gen4_lat)
+    txt = '# ***VERBATIM_STRING***'
+
+    # working directory for the test
+    # https://docs.pytest.org/en/6.2.x/tmpdir.html
+    wd = tmpdir
+    import os.path
+    fn_lat = os.path.abspath(wd.join('./lat.lat'))
+
+    # write lattice including test string for verbatim functionality (note '\n' at the end)
+    write_gen4_lat({'LX':sase_lat,'V':txt+'\n'}, filepath=fn_lat)
+    # arriving here, we know that no exception was raised -> already good
+
+    ### NOW: verify that string really made it to lattice file
+    with open(fn_lat, 'r') as fin:
+        contents = fin.read()
+        found_string = txt in contents
+
+    assert found_string, 'test string not found in lattice file'

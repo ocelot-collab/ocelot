fixed sign of survey function. Minor change in physics proc. Twiss class now check gamma and calculate if beta is given

ocelot/cpbd/magnetic_lattice.py
@@ -351,7 +351,9 @@ class MagneticLattice:
 
     def survey(self, x0=0, y0=0, z0=0, ang_x=0.0, ang_y=0.0):
         """
-        Function calculates coordinates in rectangular coordinates system at the beginning of each element in lattice
+        Function calculates coordinates in rectangular coordinates system at the beginning of each element in lattice.
+        we use convention from MAD8 where "a positive bend angle represents a bend to the right,
+        i.e. towards negative x vales" https://project-madwindows.web.cern.ch/MAD-resources/MAD8.13%20User%20Reference%20Manual.pdf
         :param x0: 0, initial offset in x direction
         :param y0: 0, initial offset in y direction
         :param z0: 0, initial offset in z direction
@@ -366,14 +368,14 @@ class MagneticLattice:
         a_y = [ang_y]
         for e in self.sequence:
             if e.__class__ in [Bend, SBend, RBend] and e.angle != 0.:
-                ang_x += e.angle * 0.5 * np.cos(e.tilt)
-                ang_y += e.angle * 0.5 * np.sin(e.tilt)
+                ang_x += -e.angle * 0.5 * np.cos(e.tilt)
+                ang_y += -e.angle * 0.5 * np.sin(e.tilt)
                 s = 2 * e.l * np.sin(e.angle * 0.5) / e.angle
                 x0 += s * np.sin(ang_x)
                 y0 += s * np.sin(ang_y)
                 z0 += s * np.cos(np.sqrt(ang_x ** 2 + ang_y ** 2))
-                ang_x += e.angle * 0.5 * np.cos(e.tilt)
-                ang_y += e.angle * 0.5 * np.sin(e.tilt)
+                ang_x += -e.angle * 0.5 * np.cos(e.tilt)
+                ang_y += -e.angle * 0.5 * np.sin(e.tilt)
             else:
                 x0 += e.l * np.sin(ang_x)
                 y0 += e.l * np.sin(ang_y)

ocelot/cpbd/physics_proc.py
@@ -489,22 +489,27 @@ class SpontanRadEffects(PhysProc):
     energy loss and quantum diffusion
     """
 
-    def __init__(self, K=0.0, lperiod=0.0, type="planar"):
+    def __init__(self, K=0.0, lperiod=0.0, type="planar", **kwargs):
         """
+        Initialize spontaneous radiation effects.
 
-        :param Kx: Undulator deflection parameter
-        :param lperiod: undulator period in [m]
+        :param K: Undulator deflection parameter
+        :param lperiod: Undulator period in meters
         :param type: "planar"/"helical" undulator or "dipole"
-        :param radius: np.inf,  in case of type = "dipole", one must specify a dipole radius
+        :param kwargs: Additional keyword arguments for customization
         """
-        PhysProc.__init__(self)
+        super().__init__(**kwargs)  # Pass any extra arguments to the parent class
+
+        # Explicitly defined parameters
         self.K = K
         self.lperiod = lperiod
         self.type = type
-        self.energy_loss = True
-        self.quant_diff = True
-        self.filling_coeff = 1.0
-        self.radius = np.inf
+
+        # Optional parameters with default values, can be overridden by kwargs
+        self.energy_loss = kwargs.get("energy_loss", True)
+        self.quant_diff = kwargs.get("quant_diff", True)
+        self.filling_coeff = kwargs.get("filling_coeff", 1.0)
+        self.radius = kwargs.get("radius", np.inf)
 
     def apply(self, p_array, dz):
         _logger.debug("SpontanRadEffects: apply")
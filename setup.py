55aa579468147477a99041b91b409c325413e29e
sergey-tomin <tomin.sergey@gmail.com>
2025-07-17 22:27:37 +0200

    Add long_description for PyPI

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding='utf-8')
 
 setup(
     name='ocelot-collab',
     version='25.07.0',
     description='Accelerator, radiation and x-ray optics simulation framework',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='ocelot-collab',
     author_email='sergey.tomin@desy.de',
     url='https://github.com/ocelot-collab/ocelot',

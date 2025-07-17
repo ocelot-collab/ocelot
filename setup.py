04b98ae927c2c444727313136af38a4d3288008b
sergey-tomin <tomin.sergey@gmail.com>
2025-07-17 16:40:48 +0200

    added toml file

--- setup.py
+++ setup.py
@@ -1,28 +1,25 @@
 from setuptools import setup, find_packages
-from os.path import join, dirname
-
-all_packages = []
-for pkg in find_packages():
-    if "demos" in pkg:
-        pkg = "ocelot." + pkg
-    all_packages.append(pkg)
-
 
 setup(
     name='ocelot',
     version='25.06.0',
     description='Accelerator, radiation and x-ray optics simulation framework',
     author='ocelot-collab',
-    author_email='tomin.sergey@gmail.com',
+    author_email='sergey.tomin@desy.de',
     url='https://github.com/ocelot-collab/ocelot',
-    packages=all_packages,
-    package_dir={'ocelot.demos': 'demos'},  ## install examples along with the rest of the source
+    packages=find_packages(include=["ocelot", "ocelot.*", "demos", "demos.*"]),
+    package_dir={
+        'ocelot': 'ocelot',
+        'demos': 'demos'
+    },
     install_requires=[
         'numpy', 'scipy', 'matplotlib', 'pandas', 'h5py'
     ],
     extras_require={'docs': ['Sphinx', 'alabaster', 'sphinxcontrib-jsmath']},
-    package_data={'ocelot.optics': ['data/*.dat'],
-                  "ocelot": ["py.typed"]},
+    package_data={
+        'ocelot.optics': ['data/*.dat'],
+        'ocelot': ['py.typed']
+    },
     license="GNU General Public License v3.0",
     python_requires=">=3.9"
 )

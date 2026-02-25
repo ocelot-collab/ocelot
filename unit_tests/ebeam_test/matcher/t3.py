2bb6fff65b43b1511eec28d23cd5223c5adb71cb
sergey-tomin <tomin.sergey@gmail.com>
2026-02-25 09:23:27 +0100

    added matcher unit tests

---
+++ unit_tests/ebeam_test/matcher/t3.py
@@ -0,0 +1,443 @@
+# Converted from component_list_2026.01.21.xls
+
+from ocelot.cpbd.elements import *
+from ocelot.cpbd.beam import Twiss
+
+twiss0 = Twiss()
+twiss0._E = 14.0
+twiss0._beta_x = 25.586
+twiss0._beta_y = 38.9113
+twiss0._alpha_x = -0.854
+twiss0._alpha_y = 1.242
+
+
+
+# Drifts:
+vcst10t22_2473_t3 = Drift(eid="VCST10T22.2473.T3")
+d_0 = Drift(l=0.2579999999998108, eid="D_0")
+d_1 = Drift(l=0.042000000000371074, eid="D_1")
+vcst22t40_2473_t3 = Drift(eid="VCST22T40.2473.T3")
+d_2 = Drift(l=1.4304999999999382, eid="D_2")
+d_3 = Drift(l=1.3099999999999454, eid="D_3")
+d_4 = Drift(l=0.22999999999974535, eid="D_4")
+d_5 = Drift(l=2.920000000000255, eid="D_5")
+d_6 = Drift(l=0.20499999999992724, eid="D_6")
+d_7 = Drift(l=0.1399999999996544, eid="D_7")
+d_8 = Drift(l=5.0300000000001095, eid="D_8")
+d_9 = Drift(l=0.9415000000000873, eid="D_9")
+d_10 = Drift(l=0.5, eid="D_10")
+vcchirp_s_2487_t3 = Drift(eid="VCCHIRP_S.2487.T3")
+d_11 = Drift(l=1.0, eid="D_11")
+vcchirp_s_2488_t3 = Drift(eid="VCCHIRP_S.2488.T3")
+d_12 = Drift(l=1.0, eid="D_12")
+vcchirp_s_2489_t3 = Drift(eid="VCCHIRP_S.2489.T3")
+d_13 = Drift(l=0.5, eid="D_13")
+d_14 = Drift(l=0.5, eid="D_14")
+vcchirp_s_2490_t3 = Drift(eid="VCCHIRP_S.2490.T3")
+d_15 = Drift(l=1.0, eid="D_15")
+vcchirp_s_2491_t3 = Drift(eid="VCCHIRP_S.2491.T3")
+d_16 = Drift(l=1.0, eid="D_16")
+vcchirp_s_2492_t3 = Drift(eid="VCCHIRP_S.2492.T3")
+d_17 = Drift(l=0.5, eid="D_17")
+d_18 = Drift(l=2.5465000000003783, eid="D_18")
+d_19 = Drift(l=0.09999999999969078, eid="D_19")
+d_20 = Drift(l=10.22500000000009, eid="D_20")
+d_21 = Drift(l=0.20499999999992724, eid="D_21")
+d_22 = Drift(l=0.1399999999996544, eid="D_22")
+d_23 = Drift(l=0.33000000000029106, eid="D_23")
+d_24 = Drift(l=9.759999999999945, eid="D_24")
+d_25 = Drift(l=0.205000000000382, eid="D_25")
+d_26 = Drift(l=0.1399999999996544, eid="D_26")
+d_27 = Drift(l=10.340000000000055, eid="D_27")
+d_28 = Drift(l=0.2104500000000371, eid="D_28")
+d_29 = Drift(l=0.1554499999999448, eid="D_29")
+d_30 = Drift(l=1.544999999999891, eid="D_30")
+d_31 = Drift(l=0.21045000000049185, eid="D_31")
+d_32 = Drift(l=0.1554499999999448, eid="D_32")
+d_33 = Drift(l=9.059999999999764, eid="D_33")
+d_34 = Drift(l=0.18500000000021827, eid="D_34")
+d_35 = Drift(l=0.1819999999996071, eid="D_35")
+d_36 = Drift(l=0.21845000000030268, eid="D_36")
+d_37 = Drift(l=0.135449999999963, eid="D_37")
+vcst40t93x_2544_t3 = Drift(eid="VCST40T93X.2544.T3")
+d_38 = Drift(l=0.25, eid="D_38")
+d_39 = Drift(l=2.499999982319423e-05, eid="D_39")
+d_40 = Drift(l=2.500000027794158e-05, eid="D_40")
+vcst93xt40_2547_t3 = Drift(eid="VCST93XT40.2547.T3")
+d_41 = Drift(l=1.0701500000000124, eid="D_41")
+d_42 = Drift(l=3.5951499999998964, eid="D_42")
+d_43 = Drift(l=0.3451500000001033, eid="D_43")
+d_44 = Drift(l=0.2901500000000603, eid="D_44")
+d_45 = Drift(l=0.6717999999998938, eid="D_45")
+d_46 = Drift(l=2.1167999999999427, eid="D_46")
+d_47 = Drift(l=0.3451500000001033, eid="D_47")
+d_48 = Drift(l=0.2901500000000603, eid="D_48")
+d_49 = Drift(l=0.8899999999996908, eid="D_49")
+d_50 = Drift(l=0.43180000000029395, eid="D_50")
+d_51 = Drift(l=1.0767999999999789, eid="D_51")
+d_52 = Drift(l=0.18999999999987266, eid="D_52")
+d_53 = Drift(l=0.3451500000001033, eid="D_53")
+d_54 = Drift(l=0.37014999999998754, eid="D_54")
+d_55 = Drift(l=2.499999982319423e-05, eid="D_55")
+d_56 = Drift(l=2.499999982319423e-05, eid="D_56")
+d_57 = Drift(l=0.875, eid="D_57")
+d_58 = Drift(l=0.21045000000049185, eid="D_58")
+d_59 = Drift(l=0.1554499999999448, eid="D_59")
+d_60 = Drift(l=9.834999999999855, eid="D_60")
+d_61 = Drift(l=0.2104500000000371, eid="D_61")
+d_62 = Drift(l=0.1554499999999448, eid="D_62")
+d_63 = Drift(l=1.544999999999891, eid="D_63")
+d_64 = Drift(l=0.2104500000000371, eid="D_64")
+d_65 = Drift(l=0.1554499999999448, eid="D_65")
+d_66 = Drift(l=18.609999999999946, eid="D_66")
+d_67 = Drift(l=0.205000000000382, eid="D_67")
+d_68 = Drift(l=0.1399999999996544, eid="D_68")
+d_69 = Drift(l=21.064999999999962, eid="D_69")
+d_70 = Drift(l=0.205000000000382, eid="D_70")
+d_71 = Drift(l=0.1399999999996544, eid="D_71")
+d_72 = Drift(l=21.064999999999962, eid="D_72")
+d_73 = Drift(l=0.205000000000382, eid="D_73")
+d_74 = Drift(l=0.1399999999996544, eid="D_74")
+d_75 = Drift(l=21.064999999999962, eid="D_75")
+d_76 = Drift(l=0.205000000000382, eid="D_76")
+d_77 = Drift(l=0.1399999999996544, eid="D_77")
+d_78 = Drift(l=13.515000000000237, eid="D_78")
+d_79 = Drift(l=0.39099999999962165, eid="D_79")
+d_80 = Drift(l=7.139000000000124, eid="D_80")
+d_81 = Drift(l=0.205000000000382, eid="D_81")
+d_82 = Drift(l=0.1399999999996544, eid="D_82")
+d_83 = Drift(l=21.064999999999962, eid="D_83")
+d_84 = Drift(l=0.205000000000382, eid="D_84")
+d_85 = Drift(l=0.1399999999996544, eid="D_85")
+d_86 = Drift(l=21.064999999999962, eid="D_86")
+d_87 = Drift(l=0.205000000000382, eid="D_87")
+d_88 = Drift(l=0.1399999999996544, eid="D_88")
+d_89 = Drift(l=9.67599999999984, eid="D_89")
+
+# Quadrupoles:
+qe_2480_t3 = Quadrupole(l=0.24, k1=0.2169588756, eid="QE.2480.T3")
+qe_2495_t3 = Quadrupole(l=0.24, k1=-0.201978927, eid="QE.2495.T3")
+qe_2506_t3 = Quadrupole(l=0.24, k1=0.2470554674, eid="QE.2506.T3")
+qe_2518_t3 = Quadrupole(l=0.24, k1=-0.25202719170000004, eid="QE.2518.T3")
+qh_2529_t3 = Quadrupole(l=1.0291, k1=0.20047962199980568, eid="QH.2529.T3")
+qh_2532_t3 = Quadrupole(l=1.0291, k1=-0.19285370480031097, eid="QH.2532.T3")
+qh_2544_t3 = Quadrupole(l=1.0291, k1=0.29487653610047615, eid="QH.2544.T3")
+qm_2549_t3 = Quadrupole(l=1.0597, k1=-0.28678140370010374, eid="QM.2549.T3")
+qm_2554_t3 = Quadrupole(l=1.0597, k1=0.2879068968000377, eid="QM.2554.T3")
+qm_2559_t3 = Quadrupole(l=1.0597, k1=-0.28678140370010374, eid="QM.2559.T3")
+qm_2564_t3 = Quadrupole(l=1.0597, k1=0.2879068968000377, eid="QM.2564.T3")
+qh_2569_t3 = Quadrupole(l=1.0291, k1=-0.2946863598999126, eid="QH.2569.T3")
+qh_2580_t3 = Quadrupole(l=1.0291, k1=0.19651887780001945, eid="QH.2580.T3")
+qh_2583_t3 = Quadrupole(l=1.0291, k1=-0.19651370810028182, eid="QH.2583.T3")
+qe_2603_t3 = Quadrupole(l=0.24, k1=0.19227955160000001, eid="QE.2603.T3")
+qe_2625_t3 = Quadrupole(l=0.24, k1=-0.19227955160000001, eid="QE.2625.T3")
+qe_2646_t3 = Quadrupole(l=0.24, k1=0.19227955160000001, eid="QE.2646.T3")
+qe_2668_t3 = Quadrupole(l=0.24, k1=-0.19227955160000001, eid="QE.2668.T3")
+qe_2690_un1 = Quadrupole(l=0.24, k1=0.19227955160000001, eid="QE.2690.UN1")
+qe_2712_un1 = Quadrupole(l=0.24, k1=-0.19227955160000001, eid="QE.2712.UN1")
+qe_2733_un1 = Quadrupole(l=0.24, k1=0.19227955160000001, eid="QE.2733.UN1")
+
+# SBends:
+be_2546_t3 = SBend(l=2.5, angle=0.0218803, e1=0.01094015, e2=0.01094015, eid="BE.2546.T3")
+be_2566_t3 = SBend(l=2.5, angle=0.0218803, e1=0.01094015, e2=0.01094015, eid="BE.2566.T3")
+
+# Sextupoles:
+saox_2555_t3 = Sextupole(l=0.3164, k2=12.57269279, eid="SAOX.2555.T3")
+sao_2561_t3 = Sextupole(l=0.3164, k2=4.465865992000632, eid="SAO.2561.T3")
+
+# Hcors:
+cmx_2477_t3 = Hcor(l=0.3, eid="CMX.2477.T3")
+cex_2480_t3 = Hcor(l=0.1, eid="CEX.2480.T3")
+cex_2507_t3 = Hcor(l=0.1, eid="CEX.2507.T3")
+chx_2530_t3 = Hcor(l=0.2, eid="CHX.2530.T3")
+chx_2542_t3 = Hcor(l=0.2, eid="CHX.2542.T3")
+chx_2554_t3 = Hcor(l=0.2, eid="CHX.2554.T3")
+chx_2562_t3 = Hcor(l=0.2, eid="CHX.2562.T3")
+chx_2581_t3 = Hcor(l=0.2, eid="CHX.2581.T3")
+cex_2603_t3 = Hcor(l=0.1, eid="CEX.2603.T3")
+cex_2647_t3 = Hcor(l=0.1, eid="CEX.2647.T3")
+cex_2690_un1 = Hcor(l=0.1, eid="CEX.2690.UN1")
+cex_2734_un1 = Hcor(l=0.1, eid="CEX.2734.UN1")
+
+# Vcors:
+cmy_2476_t3 = Vcor(l=0.3, eid="CMY.2476.T3")
+cmy_2486_t3 = Vcor(l=0.3, eid="CMY.2486.T3")
+cmy_2496_t3 = Vcor(l=0.3, eid="CMY.2496.T3")
+cmy_2507_t3 = Vcor(l=0.3, eid="CMY.2507.T3")
+cey_2518_t3 = Vcor(l=0.1, eid="CEY.2518.T3")
+chy_2533_t3 = Vcor(l=0.2, eid="CHY.2533.T3")
+chy_2543_t3 = Vcor(l=0.2, eid="CHY.2543.T3")
+chy_2559_t3 = Vcor(l=0.2, eid="CHY.2559.T3")
+chy_2569_t3 = Vcor(l=0.2, eid="CHY.2569.T3")
+chy_2584_t3 = Vcor(l=0.2, eid="CHY.2584.T3")
+cey_2625_t3 = Vcor(l=0.1, eid="CEY.2625.T3")
+cey_2668_t3 = Vcor(l=0.1, eid="CEY.2668.T3")
+cey_2712_un1 = Vcor(l=0.1, eid="CEY.2712.UN1")
+
+# Monitors:
+bpma_2480_t3 = Monitor(eid="BPMA.2480.T3")
+bpma_2487_t3 = Monitor(eid="BPMA.2487.T3")
+bpma_2493_t3 = Monitor(eid="BPMA.2493.T3")
+bpma_2506_t3 = Monitor(eid="BPMA.2506.T3")
+bpma_2517_t3 = Monitor(eid="BPMA.2517.T3")
+bpma_2528_t3 = Monitor(eid="BPMA.2528.T3")
+bpma_2531_t3 = Monitor(eid="BPMA.2531.T3")
+bpma_2543_t3 = Monitor(eid="BPMA.2543.T3")
+bpma_2553_t3 = Monitor(eid="BPMA.2553.T3")
+bpma_2558_t3 = Monitor(eid="BPMA.2558.T3")
+bpma_2563_t3 = Monitor(eid="BPMA.2563.T3")
+bpma_2568_t3 = Monitor(eid="BPMA.2568.T3")
+bpma_2579_t3 = Monitor(eid="BPMA.2579.T3")
+bpma_2582_t3 = Monitor(eid="BPMA.2582.T3")
+bpma_2603_t3 = Monitor(eid="BPMA.2603.T3")
+bpma_2624_t3 = Monitor(eid="BPMA.2624.T3")
+bpma_2646_t3 = Monitor(eid="BPMA.2646.T3")
+bpma_2668_t3 = Monitor(eid="BPMA.2668.T3")
+bpma_2690_un1 = Monitor(eid="BPMA.2690.UN1")
+bpma_2711_un1 = Monitor(eid="BPMA.2711.UN1")
+bpma_2733_un1 = Monitor(eid="BPMA.2733.UN1")
+
+# Markers:
+stsec_2473_t3 = Marker(eid="STSEC.2473.T3")
+stsub_2473_t3 = Marker(eid="STSUB.2473.T3")
+bamc_2473_t3 = Marker(eid="BAMC.2473.T3")
+tora_2475_t3 = Marker(eid="TORA.2475.T3")
+vcchirp_2490_t3 = Marker(eid="VCCHIRP.2490.T3")
+t3_csr_start = Marker(eid="t3_csr_start")
+ensub_2544_t3 = Marker(eid="ENSUB.2544.T3")
+stsub_2544_t3 = Marker(eid="STSUB.2544.T3")
+mbe_2546a_t3 = Marker(eid="MBE.2546a.T3")
+mbe_2546d_t3 = Marker(eid="MBE.2546d.T3")
+otrc_2560_t3 = Marker(eid="OTRC.2560.T3")
+mbe_2566a_t3 = Marker(eid="MBE.2566a.T3")
+mbe_2566d_t3 = Marker(eid="MBE.2566d.T3")
+t3_csr_stop = Marker(eid="t3_csr_stop")
+tora_2682_t3 = Marker(eid="TORA.2682.T3")
+ensub_2682_t3 = Marker(eid="ENSUB.2682.T3")
+ensec_2682_t3 = Marker(eid="ENSEC.2682.T3")
+stsec_2682_un1 = Marker(eid="STSEC.2682.UN1")
+ensec_2743_un1 = Marker(eid="ENSEC.2743.UN1")
+
+# Sequence:
+cell = (stsec_2473_t3,
+        stsub_2473_t3,
+        vcst10t22_2473_t3,
+        d_0,
+        bamc_2473_t3,
+        d_1,
+        vcst22t40_2473_t3,
+        d_2,
+        tora_2475_t3,
+        d_3,
+        cmy_2476_t3,
+        d_4,
+        cmx_2477_t3,
+        d_5,
+        bpma_2480_t3,
+        d_6,
+        qe_2480_t3,
+        d_7,
+        cex_2480_t3,
+        d_8,
+        cmy_2486_t3,
+        d_9,
+        bpma_2487_t3,
+        d_10,
+        vcchirp_s_2487_t3,
+        d_11,
+        vcchirp_s_2488_t3,
+        d_12,
+        vcchirp_s_2489_t3,
+        d_13,
+        vcchirp_2490_t3,
+        d_14,
+        vcchirp_s_2490_t3,
+        d_15,
+        vcchirp_s_2491_t3,
+        d_16,
+        vcchirp_s_2492_t3,
+        d_17,
+        bpma_2493_t3,
+        d_18,
+        qe_2495_t3,
+        d_19,
+        cmy_2496_t3,
+        d_20,
+        bpma_2506_t3,
+        d_21,
+        qe_2506_t3,
+        d_22,
+        cex_2507_t3,
+        d_23,
+        cmy_2507_t3,
+        d_24,
+        bpma_2517_t3,
+        d_25,
+        qe_2518_t3,
+        d_26,
+        cey_2518_t3,
+        d_27,
+        bpma_2528_t3,
+        d_28,
+        qh_2529_t3,
+        d_29,
+        chx_2530_t3,
+        d_30,
+        bpma_2531_t3,
+        d_31,
+        qh_2532_t3,
+        d_32,
+        chy_2533_t3,
+        d_33,
+        chx_2542_t3,
+        d_34,
+        chy_2543_t3,
+        d_35,
+        bpma_2543_t3,
+        d_36,
+        qh_2544_t3,
+        t3_csr_start,
+        d_37,
+        vcst40t93x_2544_t3,
+        d_38,
+        ensub_2544_t3,
+        stsub_2544_t3,
+        mbe_2546a_t3,
+        d_39,
+        be_2546_t3,
+        d_40,
+        mbe_2546d_t3,
+        vcst93xt40_2547_t3,
+        d_41,
+        qm_2549_t3,
+        d_42,
+        bpma_2553_t3,
+        d_43,
+        qm_2554_t3,
+        d_44,
+        chx_2554_t3,
+        d_45,
+        saox_2555_t3,
+        d_46,
+        bpma_2558_t3,
+        d_47,
+        qm_2559_t3,
+        d_48,
+        chy_2559_t3,
+        d_49,
+        otrc_2560_t3,
+        d_50,
+        sao_2561_t3,
+        d_51,
+        chx_2562_t3,
+        d_52,
+        bpma_2563_t3,
+        d_53,
+        qm_2564_t3,
+        d_54,
+        mbe_2566a_t3,
+        d_55,
+        be_2566_t3,
+        d_56,
+        mbe_2566d_t3,
+        d_57,
+        bpma_2568_t3,
+        d_58,
+        t3_csr_stop,
+        qh_2569_t3,
+        d_59,
+        chy_2569_t3,
+        d_60,
+        bpma_2579_t3,
+        d_61,
+        qh_2580_t3,
+        d_62,
+        chx_2581_t3,
+        d_63,
+        bpma_2582_t3,
+        d_64,
+        qh_2583_t3,
+        d_65,
+        chy_2584_t3,
+        d_66,
+        bpma_2603_t3,
+        d_67,
+        qe_2603_t3,
+        d_68,
+        cex_2603_t3,
+        d_69,
+        bpma_2624_t3,
+        d_70,
+        qe_2625_t3,
+        d_71,
+        cey_2625_t3,
+        d_72,
+        bpma_2646_t3,
+        d_73,
+        qe_2646_t3,
+        d_74,
+        cex_2647_t3,
+        d_75,
+        bpma_2668_t3,
+        d_76,
+        qe_2668_t3,
+        d_77,
+        cey_2668_t3,
+        d_78,
+        tora_2682_t3,
+        d_79,
+        ensub_2682_t3,
+        ensec_2682_t3,
+        stsec_2682_un1,
+        d_80,
+        bpma_2690_un1,
+        d_81,
+        qe_2690_un1,
+        d_82,
+        cex_2690_un1,
+        d_83,
+        bpma_2711_un1,
+        d_84,
+        qe_2712_un1,
+        d_85,
+        cey_2712_un1,
+        d_86,
+        bpma_2733_un1,
+        d_87,
+        qe_2733_un1,
+        d_88,
+        cex_2734_un1,
+        d_89,
+        ensec_2743_un1)
+
+# Power Supply IDs:
+# Quadrupole power supplies:
+qe_2480_t3.ps_id = "QE.3.T3"
+qe_2495_t3.ps_id = "QE.4.T3"
+qe_2506_t3.ps_id = "QE.5.T3"
+qe_2518_t3.ps_id = "QE.6.T3"
+qh_2529_t3.ps_id = "QH.5.T3"
+qh_2532_t3.ps_id = "QH.6.T3"
+qh_2544_t3.ps_id = "QH.1.T3"
+qm_2549_t3.ps_id = "QM.2.T3"
+qm_2554_t3.ps_id = "QM.1.T3"
+qm_2559_t3.ps_id = "QM.2.T3"
+qm_2564_t3.ps_id = "QM.1.T3"
+qh_2569_t3.ps_id = "QH.2.T3"
+qh_2580_t3.ps_id = "QH.3.T3"
+qh_2583_t3.ps_id = "QH.4.T3"
+qe_2603_t3.ps_id = "QE.1.T3"
+qe_2625_t3.ps_id = "QE.2.T3"
+qe_2646_t3.ps_id = "QE.1.T3"
+qe_2668_t3.ps_id = "QE.2.T3"
+qe_2690_un1.ps_id = "QE.1.UN1"
+qe_2712_un1.ps_id = "QE.2.UN1"
+qe_2733_un1.ps_id = "QE.1.UN1"
+
+# SBend power supplies:
+be_2546_t3.ps_id = "BE.1.T3"
+be_2566_t3.ps_id = "BE.1.T3"
+
+# Sextupole power supplies:
+saox_2555_t3.ps_id = "SAOX.1.T3"
+sao_2561_t3.ps_id = "SAO.2.T3"
